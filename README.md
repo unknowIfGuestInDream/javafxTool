a15051f879cde726c94edd57068f6f0cdb7e37e5
unknowIfGuestInDream <tang97155@163.com>
2022-10-16 21:30:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## 介绍
+javafx工具包，JDK17 + JavaFX17 + controlsfx 11.x.x
+
+## 贡献者
+
+
+## 联系
+如有问题，请发邮件到 tang97155@gmail.com
---
+++ frame/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.tlcsdm.frame {
+
+    requires transitive javafx.controls;
+    requires transitive javafx.web;
+
+    exports com.tlcsdm.frame;
+    exports com.tlcsdm.frame.model;
+
+    uses com.tlcsdm.frame.FXSamplerProject;
+    uses com.tlcsdm.frame.FXSamplerConfiguration;
+}
