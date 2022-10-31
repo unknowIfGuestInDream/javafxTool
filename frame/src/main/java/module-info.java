1e8f55f0eae1553b13288100ddb9b47ca6b8b401
梦里不知身是客 <tang97155@gmail.com>
2022-10-31 21:56:45 +0800

    fix: 修复javafx:Jlink打包镜像错误 (#42)

--- frame/src/main/java/module-info.java
+++ frame/src/main/java/module-info.java
@@ -2,16 +2,14 @@ import com.tlcsdm.frame.CenterPanelService;
 
 module com.tlcsdm.frame {
 
-	requires transitive javafx.controls;
-	requires transitive javafx.web;
-	requires tlcsdm.common;
-	requires hutool.all;
+    requires transitive javafx.controls;
+    requires transitive javafx.web;
 
-	exports com.tlcsdm.frame;
-	exports com.tlcsdm.frame.model;
+    exports com.tlcsdm.frame;
+    exports com.tlcsdm.frame.model;
 
-	uses com.tlcsdm.frame.FXSamplerProject;
-	uses com.tlcsdm.frame.MenubarConfigration;
-	uses com.tlcsdm.frame.FXSamplerConfiguration;
-	uses CenterPanelService;
+    uses com.tlcsdm.frame.FXSamplerProject;
+    uses com.tlcsdm.frame.MenubarConfigration;
+    uses com.tlcsdm.frame.FXSamplerConfiguration;
+    uses CenterPanelService;
 }
