9bbb502fadf6afd9910b78979defccb2a8200435
梦里不知身是客 <tang97155@gmail.com>
2022-11-04 10:21:18 +0800

    fix: 优化模块配置 (#49)

--- smc/src/main/java/module-info.java
+++ smc/src/main/java/module-info.java
@@ -7,7 +7,7 @@ module com.tlcsdm.smc {
     requires org.controlsfx.controls;
 
     exports com.tlcsdm.smc;
-    exports com.tlcsdm.smc.tools to com.tlcsdm.frame;
+    exports com.tlcsdm.smc.tools;
 
     provides com.tlcsdm.frame.FXSamplerProject with com.tlcsdm.smc.SmcSamplerProject;
     provides com.tlcsdm.frame.MenubarConfigration with com.tlcsdm.smc.SmcMenubarConfigration;
