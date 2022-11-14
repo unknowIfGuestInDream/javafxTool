933c7adc3300108bcbdc3b9f455e32046e464e80
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2022-11-14 11:27:38 +0800

    feat: 修改module.info文件

--- smc/src/main/java/module-info.java
+++ smc/src/main/java/module-info.java
@@ -1,16 +1,15 @@
 module com.tlcsdm.smc {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.tlcsdm.frame;
-    requires com.tlcsdm.login;
-    requires java.desktop;
-    requires org.controlsfx.controls;
+	requires javafx.fxml;
+	requires com.tlcsdm.frame;
+	requires com.tlcsdm.login;
+	requires java.desktop;
+	requires org.controlsfx.controls;
 
-    exports com.tlcsdm.smc;
-    exports com.tlcsdm.smc.tools;
+	exports com.tlcsdm.smc;
+	exports com.tlcsdm.smc.tools;
 
-    provides com.tlcsdm.frame.FXSamplerProject with com.tlcsdm.smc.SmcSamplerProject;
-    provides com.tlcsdm.frame.MenubarConfigration with com.tlcsdm.smc.SmcMenubarConfigration;
-    provides com.tlcsdm.frame.CenterPanelService with com.tlcsdm.smc.SmcCenterPanelService;
-    provides com.tlcsdm.login.LoginCheck with com.tlcsdm.smc.SmcLoginCheck;
+	provides com.tlcsdm.frame.FXSamplerProject with com.tlcsdm.smc.SmcSamplerProject;
+	provides com.tlcsdm.frame.MenubarConfigration with com.tlcsdm.smc.SmcMenubarConfigration;
+	provides com.tlcsdm.frame.CenterPanelService with com.tlcsdm.smc.SmcCenterPanelService;
+	provides com.tlcsdm.login.LoginCheck with com.tlcsdm.smc.SmcLoginCheck;
 }
