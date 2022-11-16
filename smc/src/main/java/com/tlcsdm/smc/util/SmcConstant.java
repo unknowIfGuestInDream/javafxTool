0a3d66110612d9e863de3dbbdf45be90c1242cd2
unknowIfGuestInDream <tang97155@163.com>
2022-11-16 21:46:53 +0800

    style: 代码style优化

--- smc/src/main/java/com/tlcsdm/smc/util/SmcConstant.java
+++ smc/src/main/java/com/tlcsdm/smc/util/SmcConstant.java
@@ -7,7 +7,19 @@ package com.tlcsdm.smc.util;
  * @date: 2022/11/16 21:11
  */
 public class SmcConstant {
-    public final static String GITHUB_PROJECT_URL = "https://github.com/unknowIfGuestInDream/javafxTool";
-    public final static String GITHUB_PROJECT_SUPPORT_URL = "https://github.com/unknowIfGuestInDream/javafxTool/issues";
-    public final static String GITHUB_PROJECT_FEEDBACK_URL = "https://github.com/unknowIfGuestInDream/javafxTool/discussions";
+    /**
+     * github项目地址
+     */
+    public static final String GITHUB_PROJECT_URL = "https://github.com/unknowIfGuestInDream/javafxTool";
+    /**
+     * 项目支持地址
+     */
+    public static final String GITHUB_PROJECT_SUPPORT_URL = "https://github.com/unknowIfGuestInDream/javafxTool/issues";
+    /**
+     * 项目讨论地址
+     */
+    public static final String GITHUB_PROJECT_FEEDBACK_URL = "https://github.com/unknowIfGuestInDream/javafxTool/discussions";
+
+    private SmcConstant() {
+    }
 }
