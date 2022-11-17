90227235a179350782e4d52f612395c9861f2795
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2022-11-17 10:09:10 +0800

    feat: configration接口实现

---
+++ smc/src/main/java/com/tlcsdm/smc/SmcConfiguration.java
@@ -0,0 +1,15 @@
+package com.tlcsdm.smc;
+
+import java.util.Objects;
+
+import com.tlcsdm.frame.FXSamplerConfiguration;
+
+public class SmcConfiguration implements FXSamplerConfiguration {
+
+	@Override
+	public String getSceneStylesheet() {
+		return Objects.requireNonNull(getClass().getResource("/com/tlcsdm/smc/fxsampler/fxsampler.css"))
+				.toExternalForm();
+	}
+
+}
