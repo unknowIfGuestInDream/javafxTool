83f1d4a733b7f04918a4b66e38ef83091f0cee11
梦里不知身是客 <tang97155@gmail.com>
2022-11-18 21:50:25 +0800

    feat: smc menubar增加图标 (#71)

--- smc/src/main/java/com/tlcsdm/smc/util/SmcConstant.java
+++ smc/src/main/java/com/tlcsdm/smc/util/SmcConstant.java
@@ -7,35 +7,39 @@ package com.tlcsdm.smc.util;
  * @date: 2022/11/16 21:11
  */
 public class SmcConstant {
-	/**
-	 * github项目地址
-	 */
-	public static final String GITHUB_PROJECT_URL = "https://github.com/unknowIfGuestInDream/javafxTool";
-	/**
-	 * 项目支持地址
-	 */
-	public static final String GITHUB_PROJECT_SUPPORT_URL = "https://github.com/unknowIfGuestInDream/javafxTool/issues";
-	/**
-	 * 项目讨论地址
-	 */
-	public static final String GITHUB_PROJECT_FEEDBACK_URL = "https://github.com/unknowIfGuestInDream/javafxTool/discussions";
-	/**
-	 * 项目许可证
-	 */
-	public static final String PROJECT_LICENSE_URL = "https://www.tlcsdm.com/LICENSE.txt";
-	/**
-	 * 项目许可证名称
-	 */
-	public static final String PROJECT_LICENSE_NAME = "MIT";
-	/**
-	 * 项目作者
-	 */
-	public static final String PROJECT_AUTHOR = "unknowIfGuestInDream";
-	/**
-	 * 项目版权声明
-	 */
-	public static final String PROJECT_COPYRIGHT = "Copyright © 2022 unknowIfGuestInDream";
+    /**
+     * github项目地址
+     */
+    public static final String GITHUB_PROJECT_URL = "https://github.com/unknowIfGuestInDream/javafxTool";
+    /**
+     * 项目支持地址
+     */
+    public static final String GITHUB_PROJECT_SUPPORT_URL = "https://github.com/unknowIfGuestInDream/javafxTool/issues";
+    /**
+     * 项目讨论地址
+     */
+    public static final String GITHUB_PROJECT_FEEDBACK_URL = "https://github.com/unknowIfGuestInDream/javafxTool/discussions";
+    /**
+     * 项目许可证
+     */
+    public static final String PROJECT_LICENSE_URL = "https://www.tlcsdm.com/LICENSE.txt";
+    /**
+     * 项目许可证名称
+     */
+    public static final String PROJECT_LICENSE_NAME = "MIT";
+    /**
+     * 项目作者
+     */
+    public static final String PROJECT_AUTHOR = "unknowIfGuestInDream";
+    /**
+     * 项目版权声明
+     */
+    public static final String PROJECT_COPYRIGHT = "Copyright © 2022 unknowIfGuestInDream";
+    /**
+     * 发布版本
+     */
+    public static final String PROJECT_RELEASE_URL = "https://github.com/unknowIfGuestInDream/javafxTool/releases";
 
-	private SmcConstant() {
-	}
+    private SmcConstant() {
+    }
 }
