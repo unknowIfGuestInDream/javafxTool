b945df7fa21ddac54a290b9568c17b3d1309b85f
梦里不知身是客 <tang97155@gmail.com>
2022-11-24 12:11:58 +0800

    feat: frame新增关闭和确认关闭功能 (#121)
    Co-authored-by: liang.tang.yk <liang.tang.yk@bp.renesas.com>

--- frame/src/main/java/module-info.java
+++ frame/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.tlcsdm.frame {
 
     requires transitive javafx.controls;
     requires transitive javafx.web;
+	requires javafx.graphics;
+	requires javafx.base;
 
     exports com.tlcsdm.frame;
     exports com.tlcsdm.frame.model;
