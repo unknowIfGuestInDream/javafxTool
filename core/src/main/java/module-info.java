47255235c74918e21fce49fd57cb290243f296dd
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2022-12-05 16:52:36 +0800

    feat: core, smc功能完善
    1.core FxAlerts国际化优化
    2.core 配置文件bug修复
    3.smc codeStyleLength120, FileDiff 国际化以及说明优化

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,17 +1,17 @@
 module com.tlcsdm.core {
-    requires javafx.graphics;
-    requires java.desktop;
-    requires java.sql;
-    requires org.apache.commons.lang3;
-    requires javafx.controls;
-    requires org.apache.commons.configuration2;
-    requires org.controlsfx.controls;
-    requires hutool.log;
-    requires commons.beanutils;
+	requires javafx.graphics;
+	requires java.desktop;
+	requires java.sql;
+	requires org.apache.commons.lang3;
+	requires javafx.controls;
+	requires org.apache.commons.configuration2;
+	requires org.controlsfx.controls;
+	requires hutool.log;
+	requires commons.beanutils;
 
-    exports com.tlcsdm.core.javafx;
-    exports com.tlcsdm.core.javafx.dialog;
-    exports com.tlcsdm.core.javafx.helper;
-    exports com.tlcsdm.core.javafx.util;
+	exports com.tlcsdm.core.javafx;
+	exports com.tlcsdm.core.javafx.dialog;
+	exports com.tlcsdm.core.javafx.helper;
+	exports com.tlcsdm.core.javafx.util;
 
 }
--- smc/src/main/java/com/tlcsdm/smc/SmcSample.java
+++ smc/src/main/java/com/tlcsdm/smc/SmcSample.java
@@ -1,54 +1,54 @@
 package com.tlcsdm.smc;
 
-import com.tlcsdm.frame.SampleBase;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
+import com.tlcsdm.core.javafx.util.Config;
+import com.tlcsdm.frame.SampleBase;
+
 public abstract class SmcSample extends SampleBase {
 
-    //项目信息
-    private static final ProjectInfo PROJECTINFO = new ProjectInfo();
+	// 项目信息
+	private static final ProjectInfo PROJECTINFO = new ProjectInfo();
 
-    @Override
-    public String getProjectName() {
-        return "smc";
-    }
+	@Override
+	public String getProjectName() {
+		return "smc";
+	}
 
-    @Override
-    public String getProjectVersion() {
-        return PROJECTINFO.getVersion();
-    }
+	@Override
+	public String getProjectVersion() {
+		return Config.JAVAFX_TOOL_VERSION;
+	}
 
-    /**
-     * 项目信息
-     */
-    private static class ProjectInfo {
+	/**
+	 * 项目信息
+	 */
+	private static class ProjectInfo {
 
-        private String version;
+		private String version;
 
-        public ProjectInfo() {
+		public ProjectInfo() {
 
-            InputStream s = getClass().getClassLoader().getResourceAsStream(
-                    "META-INF/MANIFEST.MF");
+			InputStream s = getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
 
-            try {
-                Manifest manifest = new Manifest(s);
-                Attributes attr = manifest.getMainAttributes();
-                version = attr.getValue("Implementation-Version");
-            } catch (IOException e) {
-                System.out.println("Unable to load project version for ControlsFX "
-                        + "samples project as the manifest file can't be read "
-                        + "or the Implementation-Version attribute is unavailable.");
-                version = "";
-            }
-        }
+			try {
+				Manifest manifest = new Manifest(s);
+				Attributes attr = manifest.getMainAttributes();
+				// Manifest-Version
+				version = attr.getValue("Implementation-Version");
+			} catch (IOException e) {
+				System.out.println("Unable to load project version for ControlsFX "
+						+ "samples project as the manifest file can't be read "
+						+ "or the Implementation-Version attribute is unavailable.");
+				version = "";
+			}
+		}
 
-        public String getVersion() {
-            return version;
-        }
-    }
+		public String getVersion() {
+			return version;
+		}
+	}
 }
-
