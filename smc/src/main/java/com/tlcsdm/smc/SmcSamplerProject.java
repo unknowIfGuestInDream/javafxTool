04106577a2a93b0cc011c13abda91f2ad6518976
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2022-12-08 17:02:36 +0800

    feat: smc welcomePage 优化，菜单新增查看系统配置

--- smc/src/main/java/com/tlcsdm/smc/SmcSamplerProject.java
+++ smc/src/main/java/com/tlcsdm/smc/SmcSamplerProject.java
@@ -28,107 +28,91 @@ package com.tlcsdm.smc;
 
 import com.tlcsdm.frame.FXSamplerProject;
 import com.tlcsdm.frame.model.WelcomePage;
-import com.tlcsdm.smc.util.SmcConstant;
-import javafx.beans.binding.StringBinding;
+
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import org.controlsfx.control.HyperlinkLabel;
-
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SmcSamplerProject implements FXSamplerProject {
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getProjectName() {
-        return "SMC";
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getProjectName() {
+		return "SMC";
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getSampleBasePackage() {
-        return "com.tlcsdm.smc";
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getSampleBasePackage() {
+		return "com.tlcsdm.smc";
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getModuleName() {
-        return "com.tlcsdm.smc";
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getModuleName() {
+		return "com.tlcsdm.smc";
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public WelcomePage getWelcomePage() {
-        VBox vBox = new VBox();
-        vBox.getStyleClass().add("welcomePage");
-        ImageView imgView = new ImageView();
-        imgView.setStyle("-fx-image: url('com/tlcsdm/smc/static/ControlsFX.png');");
-        StackPane pane = new StackPane();
-        pane.setPrefHeight(207);
-        pane.setStyle(
-                "-fx-background-image: url('com/tlcsdm/smc/static/bar.png');" + "-fx-background-repeat: repeat-x;");
-        pane.getChildren().add(imgView);
-        Label label = new Label();
-        label.setWrapText(true);
-        StringBuilder desc = new StringBuilder();
-        desc.append("SMC Tool is an agile development tool that helps you ");
-        desc.append("to provide really high quality UI controls and other tools to ");
-        desc.append("complement the core JavaFX distribution.");
-        desc.append("\n\n");
-        desc.append("Explore the available UI controls by clicking on the options to the left.");
-        label.setText(desc.toString());
-        label.setStyle("-fx-font-size: 1.5em; -fx-padding: 20 0 0 5;");
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public WelcomePage getWelcomePage() {
+		VBox vBox = new VBox();
+		vBox.getStyleClass().add("welcomePage");
+		ImageView imgView = new ImageView();
+		imgView.setStyle("-fx-image: url('com/tlcsdm/smc/static/ControlsFX.png');");
+		StackPane pane = new StackPane();
+		pane.setPrefHeight(207);
+		pane.setStyle(
+				"-fx-background-image: url('com/tlcsdm/smc/static/bar.png');" + "-fx-background-repeat: repeat-x;");
+		pane.getChildren().add(imgView);
+		Label label = new Label();
+		label.setWrapText(true);
+		String desc = """
+				SMC Tool is a development tool that can help you develop Smart Configurator.
 
-        TextField textField = new TextField();
-        textField.setEditable(false);
-        textField.setStyle("-fx-font-size: 1.5em; -fx-padding: 20 0 0 5;-fx-background-color: transparent;");
-        StringBuilder sb = new StringBuilder();
-        sb.append("This project is built on javaFX 17 and controlsfx 11. ");
-        sb.append("If you have some questions or ideas, Please contact me.");
-        textField.setText(sb.toString());
+				This tool is built on jdk17, javaFX 17 and controlsfx 11.
+				Explore the available UI controls by clicking on the options to the left.
+				If you have some questions or ideas, Please contact me at tang97155@gmail.com
+				""";
+		label.setText(desc.toString());
+		label.setStyle("-fx-font-size: 1.5em; -fx-padding: 20 0 0 5;");
 
-        HyperlinkLabel link = new HyperlinkLabel();
-        link.setStyle("-fx-font-size: 1.5em; -fx-padding: 20 0 0 5;");
-        link.textProperty().bind(new StringBinding() {
-            @Override
-            protected String computeValue() {
-                return "project url: [" + SmcConstant.GITHUB_PROJECT_URL + "]";
-            }
-        });
+//		HyperlinkLabel link = new HyperlinkLabel();
+//		link.setStyle("-fx-font-size: 1.5em; -fx-padding: 20 0 0 5;");
+//		link.textProperty().bind(new StringBinding() {
+//			@Override
+//			protected String computeValue() {
+//				return "project url: [" + SmcConstant.GITHUB_PROJECT_URL + "]";
+//			}
+//		});
+//
+//		// 存在调用两次的问题，先这样解决
+//		AtomicBoolean b = new AtomicBoolean(true);
+//		link.setOnAction(event -> {
+//			if (!b.get()) {
+//				b.set(true);
+//				return;
+//			}
+//			Desktop d = Desktop.getDesktop();
+//			try {
+//				URI address = new URI(SmcConstant.GITHUB_PROJECT_URL);
+//				d.browse(address);
+//				b.set(false);
+//			} catch (URISyntaxException | IOException e) {
+//				e.printStackTrace();
+//			}
+//		});
 
-        //存在调用两次的问题，先这样解决
-        AtomicBoolean b = new AtomicBoolean(true);
-        link.setOnAction(event -> {
-            if (!b.get()) {
-                b.set(true);
-                return;
-            }
-            Desktop d = Desktop.getDesktop();
-            try {
-                URI address = new URI(SmcConstant.GITHUB_PROJECT_URL);
-                d.browse(address);
-                b.set(false);
-            } catch (URISyntaxException | IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        vBox.getChildren().addAll(pane, label, textField, link);
-        return new WelcomePage("Welcome to SMC Tool!", vBox);
-    }
+		vBox.getChildren().addAll(pane, label);
+		return new WelcomePage("Welcome to SMC Tool!", vBox);
+	}
 }
