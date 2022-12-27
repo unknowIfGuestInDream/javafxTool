cb6e9f3073166a8373208212c2946cb635d9b529
梦里不知身是客 <tang97155@gmail.com>
2022-12-27 14:52:46 +0800

    feat: xml配置文件工具 (#182)
    Co-authored-by: liang.tang.yk <liang.tang.yk@bp.renesas.com>

--- core/src/main/java/com/tlcsdm/core/javafx/util/ConfigureUtil.java
+++ core/src/main/java/com/tlcsdm/core/javafx/util/ConfigureUtil.java
@@ -2,6 +2,10 @@ package com.tlcsdm.core.javafx.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.hutool.core.io.FileUtil;
 
 /**
  * @author: 唐 亮
@@ -30,6 +34,29 @@ public class ConfigureUtil {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+		return file;
+	}
+
+	public static File getConfigureXmlFile(String fileName) {
+		File file = new File(getConfigurePath(fileName));
+		if (!file.exists()) {
+			if (!file.getParentFile().exists()) {
+				file.getParentFile().mkdirs();
+			}
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if (FileUtil.isEmpty(file)) {
+			List<String> list = new ArrayList<>();
+			list.add("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			list.add("<!DOCTYPE properties SYSTEM \"http://java.sun.com/dtd/properties.dtd\">");
+			list.add("<properties>");
+			list.add("</properties>");
+			FileUtil.appendUtf8Lines(list, file);
 		}
 		return file;
 	}
