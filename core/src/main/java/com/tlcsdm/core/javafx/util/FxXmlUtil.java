73e8ac7492ad92d88dba8e563168f2a6d211d582
梦里不知身是客 <tang97155@gmail.com>
2022-12-27 14:52:46 +0800

    feat: xml配置文件工具 (#182)
    Co-authored-by: liang.tang.yk <liang.tang.yk@bp.renesas.com>

--- core/src/main/java/com/tlcsdm/core/javafx/util/FxXmlUtil.java
+++ core/src/main/java/com/tlcsdm/core/javafx/util/FxXmlUtil.java
@@ -1,5 +1,7 @@
 package com.tlcsdm.core.javafx.util;
 
+import java.io.File;
+
 import org.apache.commons.configuration2.XMLPropertiesConfiguration;
 import org.apache.commons.configuration2.builder.ReloadingFileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
@@ -8,54 +10,52 @@ import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.lang3.math.NumberUtils;
 
-import java.io.File;
-
 /**
  * @author: 唐 亮
  * @date: 2022/12/11 19:03
  */
 public class FxXmlUtil {
 
-    private static XMLPropertiesConfiguration conf;
+	private static XMLPropertiesConfiguration conf;
 
-    public static XMLPropertiesConfiguration getConfig() {
-        if (conf != null) {
-            return conf;
-        }
-        Parameters params = new Parameters();
-        File propertiesFile = ConfigureUtil.getConfigureFile("smc.xml");
-        XMLBuilderParameters xmlBuilderParameters = params.xml().setFile(propertiesFile)
-                .setEncoding("UTF-8").setListDelimiterHandler(new DefaultListDelimiterHandler(','))
-                .setThrowExceptionOnMissing(false);
-        ReloadingFileBasedConfigurationBuilder<XMLPropertiesConfiguration> builder = new ReloadingFileBasedConfigurationBuilder<>(
-                XMLPropertiesConfiguration.class).configure(xmlBuilderParameters);
-        builder.setAutoSave(true);
-        try {
-            conf = builder.getConfiguration();
-        } catch (ConfigurationException e) {
-            e.printStackTrace();
-        }
-        return conf;
-    }
+	public static XMLPropertiesConfiguration getConfig() {
+		if (conf != null) {
+			return conf;
+		}
 
-    public static void set(String key, Object value) {
-        getConfig().setProperty(key, value);
-    }
+		Parameters params = new Parameters();
+		File propertiesFile = ConfigureUtil.getConfigureXmlFile("smc.xml");
+		XMLBuilderParameters xmlBuilderParameters = params.xml().setFile(propertiesFile).setEncoding("UTF-8")
+				.setListDelimiterHandler(new DefaultListDelimiterHandler(',')).setThrowExceptionOnMissing(false);
+		ReloadingFileBasedConfigurationBuilder<XMLPropertiesConfiguration> builder = new ReloadingFileBasedConfigurationBuilder<>(
+				XMLPropertiesConfiguration.class).configure(xmlBuilderParameters);
+		builder.setAutoSave(true);
+		try {
+			conf = builder.getConfiguration();
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+		return conf;
+	}
 
-    public static String get(String key, String def) {
-        Object value = getConfig().getProperty(key);
-        return value == null ? def : value.toString();
-    }
+	public static void set(String key, Object value) {
+		getConfig().setProperty(key, value);
+	}
 
-    public static String get(String key) {
-        return get(key, "");
-    }
+	public static String get(String key, String def) {
+		Object value = getConfig().getProperty(key);
+		return value == null ? def : value.toString();
+	}
 
-    public static double getDouble(String key, double def) {
-        return NumberUtils.toDouble(get(key, null), def);
-    }
+	public static String get(String key) {
+		return get(key, "");
+	}
 
-    public static boolean getBoolean(String key, boolean def) {
-        return Boolean.parseBoolean(get(key, String.valueOf(def)));
-    }
+	public static double getDouble(String key, double def) {
+		return NumberUtils.toDouble(get(key, null), def);
+	}
+
+	public static boolean getBoolean(String key, boolean def) {
+		return Boolean.parseBoolean(get(key, String.valueOf(def)));
+	}
 }
