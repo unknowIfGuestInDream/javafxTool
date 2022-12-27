72811f839e62b603652b75228616ea7375a818f9
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2022-12-27 14:50:26 +0800

    feat: xml配置文件工具

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,22 +1,23 @@
 module com.tlcsdm.core {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires java.desktop;
-    requires java.sql;
-    requires org.apache.commons.lang3;
-    requires javafx.controls;
-    requires org.apache.commons.configuration2;
-    requires org.controlsfx.controls;
-    requires hutool.log;
-    requires commons.beanutils;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires java.desktop;
+	requires java.sql;
+	requires org.apache.commons.lang3;
+	requires javafx.controls;
+	requires org.apache.commons.configuration2;
+	requires org.controlsfx.controls;
+	requires hutool.log;
+	requires hutool.core;
+	requires commons.beanutils;
 
-    opens com.tlcsdm.core.javafx.controller to javafx.fxml;
-    opens com.tlcsdm.core.javafx.view to javafx.fxml;
+	opens com.tlcsdm.core.javafx.controller to javafx.fxml;
+	opens com.tlcsdm.core.javafx.view to javafx.fxml;
 
-    exports com.tlcsdm.core.javafx;
-    exports com.tlcsdm.core.javafx.control;
-    exports com.tlcsdm.core.javafx.dialog;
-    exports com.tlcsdm.core.javafx.helper;
-    exports com.tlcsdm.core.javafx.util;
+	exports com.tlcsdm.core.javafx;
+	exports com.tlcsdm.core.javafx.control;
+	exports com.tlcsdm.core.javafx.dialog;
+	exports com.tlcsdm.core.javafx.helper;
+	exports com.tlcsdm.core.javafx.util;
 
 }
