330a71c8282725a84130d1dd0bfc5a7eda356feb
梦里不知身是客 <tang97155@gmail.com>
2022-12-28 09:39:41 +0800

    feat: FxXmlUtil工具类完善 (#184)

--- core/src/main/java/com/tlcsdm/core/javafx/util/FxXmlUtil.java
+++ core/src/main/java/com/tlcsdm/core/javafx/util/FxXmlUtil.java
@@ -1,6 +1,11 @@
 package com.tlcsdm.core.javafx.util;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.configuration2.XMLPropertiesConfiguration;
 import org.apache.commons.configuration2.builder.ReloadingFileBasedConfigurationBuilder;
@@ -8,7 +13,6 @@ import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.builder.fluent.XMLBuilderParameters;
 import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
-import org.apache.commons.lang3.math.NumberUtils;
 
 /**
  * @author: 唐 亮
@@ -24,7 +28,7 @@ public class FxXmlUtil {
 		}
 
 		Parameters params = new Parameters();
-		File propertiesFile = ConfigureUtil.getConfigureXmlFile("smc.xml");
+		File propertiesFile = ConfigureUtil.getConfigureXmlFile("data.xml");
 		XMLBuilderParameters xmlBuilderParameters = params.xml().setFile(propertiesFile).setEncoding("UTF-8")
 				.setListDelimiterHandler(new DefaultListDelimiterHandler(',')).setThrowExceptionOnMissing(false);
 		ReloadingFileBasedConfigurationBuilder<XMLPropertiesConfiguration> builder = new ReloadingFileBasedConfigurationBuilder<>(
@@ -52,10 +56,55 @@ public class FxXmlUtil {
 	}
 
 	public static double getDouble(String key, double def) {
-		return NumberUtils.toDouble(get(key, null), def);
+		return getConfig().getDouble(key, def);
 	}
 
 	public static boolean getBoolean(String key, boolean def) {
-		return Boolean.parseBoolean(get(key, String.valueOf(def)));
+		return getConfig().getBoolean(key, def);
+	}
+
+	public static List<Object> getList(String key, List<String> def) {
+		return getConfig().getList(key, def);
+	}
+
+	public static void del(String key) {
+		getConfig().clearProperty(key);
+	}
+
+	public static void del() {
+		getConfig().clear();
+	}
+
+	public static List<String> getKeys(String prefix) {
+		Iterator<String> iter = getConfig().getKeys(prefix);
+		List<String> list = new ArrayList<>();
+		iter.forEachRemaining(list::add);
+		return list;
+	}
+
+	public static Map<String, Object> getValues(String prefix) {
+		Iterator<String> iter = getConfig().getKeys(prefix);
+		Map<String, Object> map = new HashMap<>();
+		while (iter.hasNext()) {
+			String key = iter.next();
+			map.put(key, get(key));
+		}
+		return map;
+	}
+
+	public static void delKeys(String prefix) {
+		Iterator<String> iter = getConfig().getKeys(prefix);
+		while (iter.hasNext()) {
+			String key = iter.next();
+			del(key);
+		}
+	}
+
+	public static void setKeys(String prefix, Map<String, Object> map) {
+		Iterator<String> iter = getConfig().getKeys(prefix);
+		while (iter.hasNext()) {
+			String key = iter.next();
+			set(key, map.get(key));
+		}
 	}
 }
