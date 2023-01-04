fdc43beaffb64ec513cf46c4ff9d2cf407eddd79
梦里不知身是客 <tang97155@gmail.com>
2023-01-04 08:24:51 +0800

    feat: 增强userData xml数据结构，新增保存顺序 (#194)
    Co-authored-by: liang.tang.yk <liang.tang.yk@bp.renesas.com>

--- smc/src/main/java/com/tlcsdm/smc/SmcSample.java
+++ smc/src/main/java/com/tlcsdm/smc/SmcSample.java
@@ -1,95 +1,96 @@
 package com.tlcsdm.smc;
 
-import cn.hutool.core.util.StrUtil;
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import com.tlcsdm.core.javafx.util.Config;
 import com.tlcsdm.core.javafx.util.FxXmlUtil;
 import com.tlcsdm.frame.SampleBase;
+
+import cn.hutool.core.util.StrUtil;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBoxBase;
 import javafx.scene.control.TextInputControl;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
 public abstract class SmcSample extends SampleBase {
 
-    protected Map<String, Object> userData = new HashMap<>();
+	protected Map<String, Object> userData = new LinkedHashMap<>();
 
-    @Override
-    public String getProjectName() {
-        return "smc";
-    }
+	@Override
+	public String getProjectName() {
+		return "smc";
+	}
 
-    @Override
-    public String getProjectVersion() {
-        return Config.JAVAFX_TOOL_VERSION;
-    }
+	@Override
+	public String getProjectVersion() {
+		return Config.JAVAFX_TOOL_VERSION;
+	}
 
-    @Override
-    public void initialize() {
-        initializeUserData();
-    }
+	@Override
+	public void initialize() {
+		initializeUserData();
+	}
 
-    /**
-     * Because initialize() is called after getPanel()
-     * so userData needs to be initialized in getPanel()
-     */
-    protected void initializeUserData() {
-        if (!FxXmlUtil.hasKey(getSampleXmlPrefix(), "id")) {
-            return;
-        }
-        userData.forEach((key, value) -> {
-            String k = getSampleXmlPrefix() + "." + key;
-            String val = FxXmlUtil.get(k, "");
-            if (value instanceof FileChooser v) {
-                v.setInitialDirectory(new File(val));
-            } else if (value instanceof DirectoryChooser v) {
-                v.setInitialDirectory(new File(val));
-            } else if (value instanceof TextInputControl v) {
-                v.setText(val);
-            } else if (value instanceof CheckBox v) {
-                v.setSelected(Boolean.parseBoolean(val));
-            } else if (value instanceof ComboBoxBase v) {
-                v.setValue(val);
-            } else {
-                // do nothing
-            }
-        });
-    }
+	/**
+	 * Because initialize() is called after getPanel() so userData needs to be
+	 * initialized in getPanel()
+	 */
+	protected void initializeUserData() {
+		if (!FxXmlUtil.hasKey(getSampleXmlPrefix(), "id")) {
+			return;
+		}
+		userData.forEach((key, value) -> {
+			String k = getSampleXmlPrefix() + "." + key;
+			String val = FxXmlUtil.get(k, "");
+			if (value instanceof FileChooser v) {
+				v.setInitialDirectory(new File(val));
+			} else if (value instanceof DirectoryChooser v) {
+				v.setInitialDirectory(new File(val));
+			} else if (value instanceof TextInputControl v) {
+				v.setText(val);
+			} else if (value instanceof CheckBox v) {
+				v.setSelected(Boolean.parseBoolean(val));
+			} else if (value instanceof ComboBoxBase v) {
+				v.setValue(val);
+			} else {
+				// do nothing
+			}
+		});
+	}
 
-    /**
-     * Manually call the current method after the function completes
-     * For example manually calling the current method after clicking the generate button
-     */
-    protected void bindUserData() {
-        if (userData.size() == 0 || StrUtil.isEmpty(getSampleId())) {
-            return;
-        }
-        FxXmlUtil.set(getSampleXmlPrefix(), "id", getSampleId());
-        FxXmlUtil.set(getSampleXmlPrefix(), "version", getSampleVersion());
-        userData.forEach((key, value) -> {
-            String k = getSampleXmlPrefix() + "." + key;
-            if (value instanceof FileChooser v) {
-                FxXmlUtil.set(k, v.getInitialDirectory());
-            } else if (value instanceof DirectoryChooser v) {
-                FxXmlUtil.set(k, v.getInitialDirectory());
-            } else if (value instanceof TextInputControl v) {
-                FxXmlUtil.set(k, v.getText());
-            } else if (value instanceof CheckBox v) {
-                FxXmlUtil.set(k, v.isSelected());
-            } else if (value instanceof ComboBoxBase v) {
-                FxXmlUtil.set(k, v.getValue());
-            } else {
-                // do nothing
-            }
-        });
-    }
+	/**
+	 * Manually call the current method after the function completes For example
+	 * manually calling the current method after clicking the generate button
+	 */
+	protected void bindUserData() {
+		if (userData.size() == 0 || StrUtil.isEmpty(getSampleId())) {
+			return;
+		}
+		FxXmlUtil.set(getSampleXmlPrefix(), "id", getSampleId());
+		FxXmlUtil.set(getSampleXmlPrefix(), "version", getSampleVersion());
+		userData.forEach((key, value) -> {
+			String k = getSampleXmlPrefix() + "." + key;
+			if (value instanceof FileChooser v) {
+				FxXmlUtil.set(k, v.getInitialDirectory());
+			} else if (value instanceof DirectoryChooser v) {
+				FxXmlUtil.set(k, v.getInitialDirectory());
+			} else if (value instanceof TextInputControl v) {
+				FxXmlUtil.set(k, v.getText());
+			} else if (value instanceof CheckBox v) {
+				FxXmlUtil.set(k, v.isSelected());
+			} else if (value instanceof ComboBoxBase v) {
+				FxXmlUtil.set(k, v.getValue());
+			} else {
+				// do nothing
+			}
+		});
+	}
 
-    protected String getSampleXmlPrefix() {
-        return getProjectName() + "." + getSampleId();
-    }
+	protected String getSampleXmlPrefix() {
+		return getProjectName() + "." + getSampleId();
+	}
 
 }
