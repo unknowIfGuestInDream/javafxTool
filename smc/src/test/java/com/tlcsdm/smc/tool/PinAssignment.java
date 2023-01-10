3899d4d74b80d13c175f4f38e928999b3f1a80a2
梦里不知身是客 <tang97155@gmail.com>
2023-01-10 19:48:01 +0800

    feat: pin 内容处理 (#204)
    Co-authored-by: liang.tang.yk <liang.tang.yk@bp.renesas.com>

---
+++ smc/src/test/java/com/tlcsdm/smc/tool/PinAssignment.java
@@ -0,0 +1,107 @@
+package com.tlcsdm.smc.tool;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.poi.excel.BigExcelWriter;
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.cell.CellLocation;
+import cn.hutool.poi.excel.cell.CellUtil;
+
+/**
+ * pin数据整理
+ */
+public class PinAssignment {
+
+	// excel的父级目录路径
+	private final String parentDirectoryPath = "C:\\workspace\\test";
+	// excel文件名称
+	private final String excelName = "E02_01_List_of_Pin_Assignment.xlsx";
+
+	private static int startX;
+	private static int startY;
+	private static int endX;
+	private static int endY;
+
+	private static int pinStartX;
+	private static int pinStartY;
+	private static int pinEndX;
+	private static int pinEndY;
+
+	private static final int deviceNum = 11;
+
+	@BeforeAll
+	public static void init() {
+		CellLocation cellLocation = ExcelUtil.toLocation("AR6");
+		startX = cellLocation.getX();
+		startY = cellLocation.getY();
+		CellLocation cellLocation1 = ExcelUtil.toLocation("BW773");
+		endX = cellLocation1.getX();
+		endY = cellLocation1.getY();
+
+		CellLocation cellLocation2 = ExcelUtil.toLocation("T6");
+		pinStartX = cellLocation2.getX();
+		pinStartY = cellLocation2.getY();
+		CellLocation cellLocation3 = ExcelUtil.toLocation("AD773");
+		pinEndX = cellLocation3.getX();
+		pinEndY = cellLocation3.getY();
+	}
+
+	@Test
+	public void deal() {
+		File file = FileUtil.file(parentDirectoryPath, excelName);
+		ExcelReader reader = ExcelUtil.getReader(file, "data");
+
+		List<List<String>> putList = new ArrayList<>();
+		for (int i = startY; i <= endY; i++) {
+			List<String> l = new ArrayList<>();
+			for (int j = startX; j <= endX; j++) {
+				l.add(valueOf(CellUtil.getCellValue(reader.getCell(j, i))));
+			}
+			putList.add(l);
+		}
+
+		List<List<String>> pinList = new ArrayList<>();
+		for (int i = pinStartY; i <= pinEndY; i++) {
+			List<String> l = new ArrayList<>();
+			for (int j = pinStartX; j <= pinEndX; j++) {
+				l.add(valueOf(CellUtil.getCellValue(reader.getCell(j, i))));
+			}
+			pinList.add(l);
+		}
+		reader.close();
+
+		BigExcelWriter excelWriter = ExcelUtil.getBigWriter(file, "result");
+
+		for (int i = 0; i < putList.size(); i++) {
+			List<String> l = putList.get(i);
+			for (int j = 0; j < l.size(); j++) {
+				if ("TAUD0I0".equals(l.get(j))) {
+					List<String> p = pinList.get(i);
+					for (int k = 0; k < p.size(); k++) {
+						if (StrUtil.isNotEmpty(p.get(k))) {
+							System.out.println(p.get(k));
+							excelWriter.writeCellValue(k + 1, 2, p.get(k));
+						}
+					}
+				}
+			}
+		}
+		File result = FileUtil.newFile(parentDirectoryPath + "\\pinAssignment\\result.xlsx");
+		excelWriter.flush(result);
+		excelWriter.close();
+
+	}
+
+	private static String valueOf(Object obj) {
+		return (obj == null) ? "" : obj.toString();
+	}
+
+}
