67f3af8cd841310aacb1e73da9ab00d1fa902394
梦里不知身是客 <tang97155@gmail.com>
2023-01-11 20:45:01 +0800

    feat: 新增加密解密工具，新增测试文件资源 (#213)
    Signed-off-by: unknowIfGuestInDream <tang97155@163.com>
    
    Signed-off-by: unknowIfGuestInDream <tang97155@163.com>

--- smc/src/test/java/com/tlcsdm/smc/tool/CompareTest.java
+++ smc/src/test/java/com/tlcsdm/smc/tool/CompareTest.java
@@ -1,5 +1,6 @@
 package com.tlcsdm.smc.tool;
 
+import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.core.lang.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +11,14 @@ import org.junit.jupiter.api.Test;
 public class CompareTest {
 
     @Test
-    public void GirretTest1() {
+    public void CompareTest1() {
         String startDate = "2022-12-07 00:00:00";
         String submit = "2022-12-15 12:00:00";
         Assert.isTrue(startDate.compareTo(submit) == -1);
+    }
+
+    @Test
+    public void resource2() {
+        System.out.println(ResourceUtil.getResource("u2a_DTS_Transfer_request_Table.xlsx").getPath());
     }
 }
