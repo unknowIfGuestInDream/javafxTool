c0429cc2bd49a70ade7643ecc3ecc15314a80891
梦里不知身是客 <tang97155@gmail.com>
2023-01-19 18:05:10 +0800

    feat: 新增DtsTriggerSourceXml组件 (#237)
    Co-authored-by: liang.tang.yk <liang.tang.yk@bp.renesas.com>

--- frame/src/main/java/com/tlcsdm/frame/model/Project.java
+++ frame/src/main/java/com/tlcsdm/frame/model/Project.java
@@ -25,13 +25,15 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.tlcsdm.frame.model;
+
+import com.tlcsdm.core.util.CoreUtil;
 import com.tlcsdm.frame.Sample;
 
 /**
  * Represents a project such as ControlsFX or JFXtras
  */
 public class Project {
-    
+
     private final String name;
     private final String basePackage;
 
@@ -41,7 +43,7 @@ public class Project {
     private String moduleName;
     // Pojo that holds the welcome tab content and title
     private WelcomePage welcomePage;
-    
+
     public Project(String name, String basePackage) {
         this.name = name;
         this.basePackage = basePackage;
@@ -52,7 +54,7 @@ public class Project {
         // convert something like 'org.controlsfx.samples.actions' to 'samples.actions'
         String packagesWithoutBase = "";
         try {
-            if (! basePackage.equals(packagePath)) {
+            if (!basePackage.equals(packagePath)) {
                 packagesWithoutBase = packagePath.substring(basePackage.length() + 1);
             }
         } catch (StringIndexOutOfBoundsException e) {
@@ -60,27 +62,26 @@ public class Project {
             e.printStackTrace();
             return;
         }
-        
+
         // then split up the packages into separate strings
-        String[] packages = packagesWithoutBase.isEmpty() ? new String[] { } : packagesWithoutBase.split("\\.");
-        
+        String[] packages = packagesWithoutBase.isEmpty() ? new String[] {} : packagesWithoutBase.split("\\.");
+
         // then for each package convert to a prettier form
         for (int i = 0; i < packages.length; i++) {
             String packageName = packages[i];
             if (packageName.isEmpty()) {
                 continue;
             }
-            
-            packageName = packageName.substring(0, 1).toUpperCase() + packageName.substring(1);
+            packageName = CoreUtil.toUpperCase4Index(packageName);
             packageName = packageName.replace("_", " ");
             packages[i] = packageName;
         }
-        
+
         // now we have the pretty package names, we add this sample into the
         // tree in the appropriate place
         sampleTree.addSample(packages, sample);
     }
-    
+
     public SampleTree getSampleTree() {
         return sampleTree;
     }
@@ -94,18 +95,19 @@ public class Project {
     }
 
     public void setWelcomePage(WelcomePage welcomePage) {
-        if(null != welcomePage) {
+        if (null != welcomePage) {
             this.welcomePage = welcomePage;
         }
     }
-    
+
     public WelcomePage getWelcomePage() {
         return this.welcomePage;
     }
 
-    @Override public String toString() {
+    @Override
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        
+
         sb.append("Project [ name: ");
         sb.append(name);
         sb.append(", sample count: ");
@@ -113,7 +115,7 @@ public class Project {
         sb.append(", tree: ");
         sb.append(sampleTree);
         sb.append(" ]");
-        
+
         return sb.toString();
     }
 }
