9f97719f2dd66ee1a91d94386c14140dceb390ca
梦里不知身是客 <tang97155@gmail.com>
2023-01-19 18:05:10 +0800

    feat: 新增DtsTriggerSourceXml组件 (#237)
    Co-authored-by: liang.tang.yk <liang.tang.yk@bp.renesas.com>

--- smc/src/main/java/module-info.java
+++ smc/src/main/java/module-info.java
@@ -19,6 +19,7 @@ module com.tlcsdm.smc {
     exports com.tlcsdm.smc;
     exports com.tlcsdm.smc.tools;
     exports com.tlcsdm.smc.unitTest;
+    exports com.tlcsdm.smc.codeDev;
 
     provides com.tlcsdm.frame.FXSamplerProject with com.tlcsdm.smc.SmcSamplerProject;
     provides com.tlcsdm.frame.MenubarConfigration with com.tlcsdm.smc.SmcMenubarConfigration;
