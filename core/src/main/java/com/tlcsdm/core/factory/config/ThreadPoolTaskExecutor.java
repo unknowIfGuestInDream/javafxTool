028d2c5794a873dfa979513fcb0eba25985892b5
unknowIfGuestInDream <tang97155@163.com>
2023-02-05 09:40:16 +0800

    feat: 新增InitializingFactory初始化接口以及线程池初始化实现
    Signed-off-by: unknowIfGuestInDream <tang97155@163.com>

---
+++ core/src/main/java/com/tlcsdm/core/factory/config/ThreadPoolTaskExecutor.java
@@ -0,0 +1,32 @@
+package com.tlcsdm.core.factory.config;
+
+import com.tlcsdm.core.factory.InitializingFactory;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: unknowIfGuestInDream
+ * @date: 2023/2/5 8:36
+ */
+public class ThreadPoolTaskExecutor implements InitializingFactory {
+    public static ThreadPoolExecutor CoreThreadPool;
+
+    @Override
+    public void initialize() throws Exception {
+        CoreThreadPool = new ThreadPoolExecutor(2,
+                50,
+                30,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(200),
+                new BasicThreadFactory.Builder()
+                        .namingPattern("sample-").build(),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+    }
+
+    public static ThreadPoolExecutor get() {
+        return CoreThreadPool;
+    }
+}
