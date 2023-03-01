7f4cf396fee7e7172456e830dfbf58d4abdc5c95
梦里不知身是客 <tang97155@gmail.com>
2023-03-01 21:45:33 +0800

    feat: HconvertExcel完成 (#307)
    Signed-off-by: unknowIfGuestInDream <tang97155@163.com>

--- core/src/main/java/com/tlcsdm/core/javafx/util/Config.java
+++ core/src/main/java/com/tlcsdm/core/javafx/util/Config.java
@@ -1,8 +1,5 @@
 package com.tlcsdm.core.javafx.util;
 
-import java.io.File;
-import java.util.Locale;
-
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.ReloadingFileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
@@ -10,6 +7,9 @@ import org.apache.commons.configuration2.builder.fluent.PropertiesBuilderParamet
 import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.lang3.math.NumberUtils;
+
+import java.io.File;
+import java.util.Locale;
 
 /**
  * 存取框架配置
@@ -20,11 +20,12 @@ public class Config {
 
     public static final String CONFIG_FILE_NAME = "systemConfigure.properties";
     public static final String USERDATA_FILE_NAME = "data.xml";
-
-    public static Locale defaultLocale = Locale.getDefault();// 设置系统语言
-
-    public static final String JAVAFX_TOOL_VERSION = "1.0.0";// JavaFxTool版本信息
-    public static final String JAVAFX_TOOL_PUBLISHDATE = "2023-02-28";// 发布日期
+    // 设置系统语言
+    public static Locale defaultLocale = Locale.getDefault();
+    // JavaFxTool版本信息
+    public static final String JAVAFX_TOOL_VERSION = "1.0.0";
+    // 发布日期
+    public static final String JAVAFX_TOOL_PUBLISHDATE = "2023-03-01";
 
     public enum Keys {
         MainWindowWidth, MainWindowHeight, MainWindowTop, MainWindowLeft, Locale, NotepadEnabled,
