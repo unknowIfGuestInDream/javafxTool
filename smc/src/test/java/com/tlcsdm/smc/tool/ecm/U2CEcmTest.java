de1af79dea94ce72da7eb84697df10525d7eb6d7
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2023-03-21 15:25:19 +0800

    feat: 处理ECM U2A

--- smc/src/test/java/com/tlcsdm/smc/tool/ecm/U2CEcmTest.java
+++ smc/src/test/java/com/tlcsdm/smc/tool/ecm/U2CEcmTest.java
@@ -27,22 +27,29 @@
 
 package com.tlcsdm.smc.tool.ecm;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.tlcsdm.core.util.FreemarkerUtil;
+
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.IORuntimeException;
 import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
-import com.tlcsdm.core.util.FreemarkerUtil;
 import freemarker.template.Configuration;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
 
 public class U2CEcmTest {
 
@@ -64,21 +71,29 @@ public class U2CEcmTest {
     @Test
     public void dealData() {
         // 输入值获取
-        String parentDirectoryPath = "C:\\workspace\\test\\ecm";
+        String parentDirectoryPath = "C:\\workspace\\test\\ecmSpec";
         String excelName = "ErrorSource.xlsx";
         String outputPath = "C:\\workspace\\test";
+
+        String categorySheetName = "Category";
+        int categoryStartRow = 2;
+        String categorys = """
+                categoryId;F
+                categoryEnName;G
+                categoryJpName;H
+                """;
+
         String resultPath = outputPath + "\\ecm";
-        String sheetName = "U2A";
+        String deviceSheetName = "U2A";
         int startRow = 3;
         String functions = """
-                Error flag set;F
-                Maskable Interrupt (EI level);G
-                Maskable Interrupt (FE level);H
-                Internal reset generation;I
-                Error output;J
-                Error trigger;K
-                Delay timer start;L
-                Pseudo error generation;M
+                optMaskint;G
+                optIntg;G
+                optDCLS;G
+                optIntrg;I
+                optErroroutput;J
+                optErrort;K
+                optDelayt;L
                 """;
         String errorSourceIdCol = "A";
         String categoryIdCol = "B";
@@ -109,59 +124,196 @@ public class U2CEcmTest {
             List<String> l = StrUtil.split(productConfig, ";");
             productMap.put(l.get(0), l.get(1));
         }
+        // category 配置数据
+        LinkedHashMap<String, String> categoryMap = new LinkedHashMap<>();
+        List<String> categoryConfigs = StrUtil.splitTrim(categorys, "\n");
+        for (String categoryConfig : categoryConfigs) {
+            List<String> l = StrUtil.split(categoryConfig, ";");
+            categoryMap.put(l.get(0), l.get(1));
+        }
+        // category 数据处理
+        ExcelReader categoryReader = ExcelUtil.getReader(FileUtil.file(parentDirectoryPath, excelName),
+                categorySheetName);
+        int categoryEndRow = categoryReader.getRowCount();
+        List<Map<String, Object>> categoryInfos = new ArrayList<>();
+        for (int i = categoryStartRow; i <= categoryEndRow; i++) {
+            Map<String, Object> categoryInfo = new HashMap<>();
+            for (String key : categoryMap.keySet()) {
+                Cell cell = categoryReader.getCell(categoryMap.get(key) + i);
+                if (cell == null) {
+                    continue;
+                }
+                String value = cell.getStringCellValue();
+                if (StrUtil.isBlank(value)) {
+                    continue;
+                }
+                categoryInfo.put(key, value);
+            }
+            if (categoryInfo.isEmpty()) {
+                continue;
+            }
+            categoryInfos.add(categoryInfo);
+        }
+        categoryReader.close();
 
-        ExcelReader reader = ExcelUtil.getReader(FileUtil.file(parentDirectoryPath, excelName), sheetName);
+        ExcelReader reader = ExcelUtil.getReader(FileUtil.file(parentDirectoryPath, excelName), deviceSheetName);
         int endRow = reader.getRowCount();
         // 清空resultPath下文件
         FileUtil.clean(resultPath);
-        //便利products
+        // 便利products
         for (String key : productMap.keySet()) {
-            Map<String, Object> paramMap = new HashMap<>();
-            List<Category> categoryInfos = new ArrayList<>();
-            List<ErrorSource> ErrorSourceInfos = new ArrayList<>();
-            paramMap.put("CategoryInfos", categoryInfos);
-            paramMap.put("ErrorSourceInfos", ErrorSourceInfos);
+            List<Map<String, Object>> ErrorSourceInfos = new ArrayList<>();
             String productCol = productMap.get(key);
-            //遍历excel sheet数据
+            // 遍历excel sheet数据
             for (int i = startRow; i <= endRow; i++) {
-                //todo 判断合并的key计算 ArrayList equals
-                if (StrUtil.isBlank(reader.getCell(errorSourceIdCol + i).getStringCellValue())) {
+                // todo 判断合并的key计算 ArrayList equals
+                Cell cell = reader.getCell(errorSourceIdCol + i);
+                if (cell == null) {
+                    continue;
+                }
+                if (StrUtil.isBlank(cell.getStringCellValue())) {
                     continue;
                 }
                 String productCondition = reader.getCell(productCol + i).getStringCellValue();
-                if ("—".equals(productCondition) || "-".equals(productCondition)) {
+                if (productCondition.contains("—") || productCondition.contains("-")) {
                     continue;
                 }
-                //todo 合并标记
+                // todo 合并标记
                 String errorSourceId = reader.getCell(errorSourceIdCol + i).getStringCellValue();
                 String categoryId = reader.getCell(categoryIdCol + i).getStringCellValue();
-                String errorSourceNumber = reader.getCell(errorSourceNumberCol + i).getStringCellValue();
+                String errorSourceNumber = String
+                        .valueOf((int) reader.getCell(errorSourceNumberCol + i).getNumericCellValue());
                 String errorSourceenName = reader.getCell(errorSourceenNameCol + i).getStringCellValue();
                 String errorSourcejpName = reader.getCell(errorSourcejpNameCol + i).getStringCellValue();
-                List<Operation> function = new ArrayList<>();
+                List<Map<String, Object>> function = new ArrayList<>();
                 for (String funcId : operationMap.keySet()) {
                     String funcCol = operationMap.get(funcId);
                     String funcSupCondition = reader.getCell(funcCol + i).getStringCellValue();
-                    //todo 存在 —*8 需要处理
-                    boolean support = !("—".equals(funcSupCondition) || "-".equals(funcSupCondition));
-                    String errorNote = "";
-                    // * 处理
-                    if (support) {
-                        if (funcSupCondition.contains("*")) {
-                            // todo
-                        }
-                    }
-                    Operation operation = new Operation(funcId, String.valueOf(support), errorNote);
+                    // support 向下判断
+                    boolean support = !(funcSupCondition.contains("—") || funcSupCondition.contains("-"));
+                    Map<String, Object> operation = new HashMap<>();
+                    operation.put("funcId", funcId);
+                    operation.put("support", String.valueOf(support));
+                    operation.put("errorNote", "");
+                    // 数据后置处理
+                    handlerOperationSupport(operation, funcSupCondition);
                     function.add(operation);
                 }
-                ErrorSource errorSource = new ErrorSource(errorSourceId, categoryId, errorSourceNumber, errorSourceenName, errorSourcejpName, function);
+                Map<String, Object> errorSource = new HashMap<>();
+                errorSource.put("errorSourceId", errorSourceId);
+                errorSource.put("categoryId", categoryId);
+                errorSource.put("errorSourceNumber", errorSourceNumber);
+                errorSource.put("errorSourceenName", errorSourceenName);
+                errorSource.put("errorSourcejpName", errorSourcejpName);
+                errorSource.put("function", function);
+                handlerErrorSourceMap(errorSource, key);
                 ErrorSourceInfos.add(errorSource);
             }
-
-            //todo 触发合并条件，中断
+            Map<String, Object> paramMap = new HashMap<>();
+            paramMap.put("categoryInfos", categoryInfos);
+            paramMap.put("errorSourceInfos", ErrorSourceInfos);
+            // todo 触发合并条件，中断
             File result = FileUtil.newFile(resultPath + "\\" + key + ".xml");
-            FileUtil.appendUtf8String(FreemarkerUtil.getTemplateContent(configuration, paramMap, getFtlPath(sheetName)), result);
+            FileUtil.appendUtf8String(
+                    FreemarkerUtil.getTemplateContent(configuration, paramMap, getFtlPath(deviceSheetName)), result);
         }
+        reader.close();
+    }
+
+    /**
+     * errorSource 数据后续处理
+     */
+    private void handlerErrorSourceMap(Map<String, Object> errorSource, String product) {
+        String errorSourceenName = (String) errorSource.get("errorSourceenName");
+        String errorSourcejpName = (String) errorSource.get("errorSourcejpName");
+        errorSourceenName = cleanErrorSourceData(errorSourceenName);
+        errorSourcejpName = cleanErrorSourceData(errorSourcejpName);
+        errorSource.put("errorSourceenName", errorSourceenName);
+        errorSource.put("errorSourcejpName", errorSourcejpName);
+
+        List<Map<String, Object>> function = (List<Map<String, Object>>) errorSource.get("function");
+        List<Map<String, Object>> extraFunc = new ArrayList<>();
+        for (Map<String, Object> map : function) {
+            String funcId = map.get("funcId").toString();
+            if ("optErrort".equals(funcId)) {
+                String support = map.get("support").toString();
+                String errorNote = map.get("errorNote").toString();
+                int size = 0;
+                if (product.startsWith("RH850U2A16")) {
+                    size = 4;
+                    generateErrort(size, support, errorNote, extraFunc);
+                    function.remove(map);
+                } else if (product.startsWith("RH850U2A8") || product.startsWith("RH850U2A6")) {
+                    size = 2;
+                    generateErrort(size, support, errorNote, extraFunc);
+                    function.remove(map);
+                } else {
+                    break;
+                }
+            }
+        }
+        function.addAll(extraFunc);
+    }
+
+    private void generateErrort(int size, String support, String errorNote, List<Map<String, Object>> extraFunc) {
+        for (int i = 0; i < size; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("funcId", "optErrort" + i);
+            map.put("support", support);
+            map.put("errorNote", errorNote);
+            extraFunc.add(map);
+        }
+    }
+
+    /**
+     *  处理使能条件的 * 信息, 默认是support = true下的
+     */
+    private void handlerOperationSupport(Map<String, Object> operation, String funcSupCondition) {
+        if (funcSupCondition.contains("*")) {
+            String mesNum = StrUtil.subAfter(funcSupCondition, "*", true);
+            if ("1".equals(mesNum) || "2".equals(mesNum)) {
+                operation.put("errorNote", mesNum);
+            }
+            if ("5".equals(mesNum)) {
+                String funcId = operation.get("funcId").toString();
+                if ("optDCLS".equals(funcId)) {
+                    operation.put("support", "true");
+                }
+                if ("optIntg".equals(funcId)) {
+                    operation.put("support", "false");
+                }
+            }
+        } else {
+            String funcId = operation.get("funcId").toString();
+            if ("optDCLS".equals(funcId)) {
+                operation.put("support", "false");
+            }
+            if ("optIntg".equals(funcId)) {
+                operation.put("support", "true");
+            }
+        }
+
+    }
+
+    /**
+     * 清洗ErrorSource数据
+     */
+    private String cleanErrorSourceData(String data) {
+        if (data.contains(" (")) {
+            data = StrUtil.replace(data, " (", "(");
+        }
+        if (data.contains("\n")) {
+            List<String> list = StrUtil.split(data, "\n");
+            data = list.get(0);
+            for (int i = 1; i < list.size(); i++) {
+                data += " ";
+                data += list.get(i).replaceFirst("- ", "-");
+            }
+        }
+        if (data.endsWith("*7")) {
+            data = StrUtil.replaceLast(data, "*7", "(For debug purpose only)");
+        }
+        return data;
     }
 
     private String getFtlPath(String sheetName) {
@@ -169,16 +321,6 @@ public class U2CEcmTest {
             return "ecm/u2a.ftl";
         }
         return null;
-    }
-
-    record Category(String categoryId, String categoryEnName, String categoryJpName) {
-    }
-
-    record Operation(String funcId, String support, String errorNote) {
-    }
-
-    record ErrorSource(String errorSourceId, String categoryId, String errorSourceNumber, String errorSourceenName,
-                       String errorSourcejpName, List<Operation> function) {
     }
 
 }
--- smc/src/test/resources/templates/ecm/u2a.ftl
+++ smc/src/test/resources/templates/ecm/u2a.ftl
@@ -2,13 +2,13 @@
 <!DOCTYPE xml>
 <ECMInfos>
     <CategoryInfos>
-<#list CategoryInfos as item>
-        <Category categoryId="${item.categoryId}" categoryEnName="${item.categoryEnName}" categoryJpName="${item.categoryJpName}"/>
+<#list categoryInfos as item>
+        <Category categoryId="${item.categoryId!''}" categoryEnName="${item.categoryEnName!''}" categoryJpName="${item.categoryJpName!''}"/>
 </#list>
     </CategoryInfos>
     <ErrorSourceInfos>
-<#list ErrorSourceInfos as item>
-        <ErrorSource errorSourceId="${item.errorSourceId}" categoryId="${item.categoryId}" errorSourceNumber="${item.errorSourceNumber}" errorSourceenName="${item.errorSourceenName}" errorSourcejpName="${item.errorSourcejpName}">
+<#list errorSourceInfos as item>
+        <ErrorSource errorSourceId="${item.errorSourceId!''}" categoryId="${item.categoryId!''}" errorSourceNumber="${item.errorSourceNumber!''}" errorSourceenName="${item.errorSourceenName!''}" errorSourcejpName="${item.errorSourcejpName!''}">
             <Function>
 <#list item.function as oper>
                 <Operation funcId="${oper.funcId}" support="${oper.support}" <#if oper.errorNote?? && oper.errorNote != "">errorNote="${oper.errorNote}"</#if>/>
