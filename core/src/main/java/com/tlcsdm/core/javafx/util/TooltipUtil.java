70e2ba66e9a5c79c141a04983b479bd84698b44b
unknowIfGuestInDream <tang97155@163.com>
2023-03-26 21:08:27 +0800

    feat: 新增日志打印控制台组件
    Signed-off-by: unknowIfGuestInDream <tang97155@163.com>

---
+++ core/src/main/java/com/tlcsdm/core/javafx/util/TooltipUtil.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2019, 2023 unknowIfGuestInDream
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.core.javafx.util;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Tooltip;
+import javafx.stage.Window;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
+import org.controlsfx.tools.Utils;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @author: unknowIfGuestInDream
+ * @date: 2023/3/26 20:59
+ */
+public class TooltipUtil {
+
+    public static void showToast(String message) {
+        showToast((Node) null, message);
+    }
+
+    public static void showToast(Node node, String message) {
+        Window window = Utils.getWindow(node);
+        double x;
+        double y;
+        if (node != null) {
+            x = GetScreenUtil.getScreenX(node) + GetScreenUtil.getWidth(node) / 2.0D;
+            y = GetScreenUtil.getScreenY(node) + GetScreenUtil.getHeight(node);
+        } else {
+            x = window.getX() + window.getWidth() / 2.0D;
+            y = window.getY() + window.getHeight();
+        }
+
+        showToast(window, message, 3000L, x, y);
+    }
+
+    public static void showToast(Window window, String message, long time, double x, double y) {
+        final Tooltip tooltip = new Tooltip(message);
+        tooltip.setAutoHide(true);
+        tooltip.setOpacity(0.9D);
+        tooltip.setWrapText(true);
+        tooltip.show(window, x, y);
+        tooltip.setAnchorX(tooltip.getAnchorX() - tooltip.getWidth() / 2.0D);
+        tooltip.setAnchorY(tooltip.getAnchorY() - tooltip.getHeight());
+        if (time > 0L) {
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Platform.runLater(tooltip::hide);
+                }
+            }, time);
+        }
+    }
+
+    public static void showToast(String message, Pos pos) {
+        showToast((String) null, message, (Node) null, 3.0D, pos, (EventHandler) null, (Object) null, true, true);
+    }
+
+    public static void showToast(String title, String message) {
+        showToast(title, message, (Node) null, 3.0D, Pos.BOTTOM_CENTER, (EventHandler) null, (Object) null, true, true);
+    }
+
+    public static void showToast(String title, String message, Pos pos) {
+        showToast(title, message, (Node) null, 3.0D, pos, (EventHandler) null, (Object) null, true, true);
+    }
+
+    public static void showToast(String title, String message, Node graphic, double hideTime, Pos pos, EventHandler<ActionEvent> onAction, Object owner, boolean isHideCloseButton, boolean isDarkStyle) {
+        Notifications notificationBuilder = Notifications.create().title(title).text(message).graphic(graphic).hideAfter(Duration.seconds(hideTime)).position(pos).onAction(onAction);
+        if (owner != null) {
+            notificationBuilder.owner(owner);
+        }
+
+        if (isHideCloseButton) {
+            notificationBuilder.hideCloseButton();
+        }
+
+        if (isDarkStyle) {
+            notificationBuilder.darkStyle();
+        }
+
+        Platform.runLater(() -> {
+            notificationBuilder.show();
+        });
+    }
+}
