3596fb8f178f369327be6fa6f4660f2d28daf8eb
梦里不知身是客 <liang.tang.cx@gmail.com>
2023-04-16 11:57:38 +0800

    feat(common): 新增common模块 (#485)
    * feat(common): 新增common模块
    
    * feat(common): 新增common模块

---
+++ common/src/main/java/com/tlcsdm/jfxcommon/util/I18nUtils.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2019, 2023 unknowIfGuestInDream
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.jfxcommon.util;
+
+import com.tlcsdm.core.javafx.util.Config;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+/**
+ * @author: unknowIfGuestInDream
+ * @date: 2022/10/22 17:49
+ */
+public class I18nUtils {
+    /**
+     * 资源包默认路径
+     */
+    public static String BASENAME = "com.tlcsdm.common.i18n.messages";
+
+    /**
+     * gets the string with the given key from the resource bundle for the current
+     * locale and uses it as first argument to MessageFormat.format, passing in the
+     * optional args and returning the result.
+     *
+     * @param key  message key
+     * @param args optional arguments for the message
+     * @return localized formatted string
+     */
+    public static String get(final String key, final Object... args) {
+        ResourceBundle bundle = ResourceBundle.getBundle(BASENAME, Config.defaultLocale);
+        return MessageFormat.format(bundle.getString(key), args);
+    }
+}
