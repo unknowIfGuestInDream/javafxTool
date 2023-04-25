8661c85308f347dd73f0fc92a8bdef56ce6d4579
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2023-04-25 10:49:26 +0800

    chore: 补充groovy依赖

---
+++ core/src/test/resources/groovy/DateDemo.groovy
@@ -0,0 +1,35 @@
+def date = LocalDate.parse('Jun 3, 04', 'MMM d, yy')
+assert date == LocalDate.of(2004, Month.JUNE, 3)
+
+def time = LocalTime.parse('4:45', 'H:mm')
+assert time == LocalTime.of(4, 45, 0)
+
+def offsetTime = OffsetTime.parse('09:47:51-1234', 'HH:mm:ssZ')
+assert offsetTime == OffsetTime.of(9, 47, 51, 0, ZoneOffset.ofHoursMinutes(-12, -34))
+
+def dateTime = ZonedDateTime.parse('2017/07/11 9:47PM Pacific Standard Time', 'yyyy/MM/dd h:mma zzzz')
+assert dateTime == ZonedDateTime.of(
+        LocalDate.of(2017, 7, 11),
+        LocalTime.of(21, 47, 0),
+        ZoneId.of('America/Los_Angeles')
+)
+
+def aprilFools = LocalDate.of(2018, Month.APRIL, 1)
+
+def nextAprilFools = aprilFools + Period.ofDays(365) // add 365 days
+assert nextAprilFools.year == 2019
+
+def idesOfMarch = aprilFools - Period.ofDays(17) // subtract 17 days
+assert idesOfMarch.dayOfMonth == 15
+assert idesOfMarch.month == Month.MARCH
+
+def date = LocalDate.of(2018, Month.MARCH, 12)
+assert date[ChronoField.YEAR] == 2018
+assert date[ChronoField.MONTH_OF_YEAR] == Month.MARCH.value
+assert date[ChronoField.DAY_OF_MONTH] == 12
+assert date[ChronoField.DAY_OF_WEEK] == DayOfWeek.MONDAY.value
+
+def period = Period.ofYears(2).withMonths(4).withDays(6)
+assert period[ChronoUnit.YEARS] == 2
+assert period[ChronoUnit.MONTHS] == 4
+assert period[ChronoUnit.DAYS] == 6
