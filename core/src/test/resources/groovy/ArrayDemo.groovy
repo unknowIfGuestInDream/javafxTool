6b24f5d2b878cdcc3b777bfe82f985be5a501f49
梦里不知身是客 <liang.tang.cx@gmail.com>
2023-04-25 14:03:58 +0800

    test: 新增groovy示例 (#514)

---
+++ core/src/test/resources/groovy/ArrayDemo.groovy
@@ -0,0 +1,39 @@
+// 数组
+String[] arrStr = ['Ananas', 'Banana', 'Kiwi']
+
+assert arrStr instanceof String[]
+assert !(arrStr instanceof List)
+
+def numArr = [1, 2, 3] as int[]
+
+assert numArr instanceof int[]
+assert numArr.size() == 3
+
+// 多维数组
+def matrix3 = new Integer[3][3]
+assert matrix3.size() == 3
+
+Integer[][] matrix2
+matrix2 = [[1, 2], [3, 4]]
+assert matrix2 instanceof Integer[][]
+
+// 您可以定义新数组的边界
+// 或者声明一个数组而不指定其边界
+String[] names = ['Cédric', 'Guillaume', 'Jochen', 'Paul']
+assert names[0] == 'Cédric'
+
+names[2] = 'Blackdrag'
+assert names[2] == 'Blackdrag'
+
+// java风格数组初始化
+def primes = new int[] {2, 3, 5, 7, 11}
+assert primes.size() == 5 && primes.sum() == 28
+assert primes.class.name == '[I'
+
+def pets = new String[] {'cat', 'dog'}
+assert pets.size() == 2 && pets.sum() == 'catdog'
+assert pets.class.name == '[Ljava.lang.String;'
+
+// traditional Groovy alternative still supported
+String[] groovyBooks = [ 'Groovy in Action', 'Making Java Groovy' ]
+assert groovyBooks.every{ it.contains('Groovy') }
