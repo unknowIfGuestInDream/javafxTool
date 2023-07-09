2fca3c840c859e7d5d991d6dc8c6cd4a835d24fa
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2023-07-09 16:07:38 +0800

    feat: FileChooserUtil

---
+++ core/src/main/java/com/tlcsdm/core/javafx/helper/DropContentHelper.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2023 unknowIfGuestInDream
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.core.javafx.helper;
+
+import javafx.scene.Node;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+
+import java.io.File;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+
+/**
+ * @author: unknowIfGuestInDream
+ * @date: 2023/7/9 14:57
+ */
+public class DropContentHelper {
+    private DropContentHelper() {
+    }
+
+    public static <T extends Node> void acceptText(T node, BiConsumer<T, String> onAccept) {
+        node.setOnDragOver((event) -> {
+            Dragboard board = event.getDragboard();
+            if (!board.hasString() && !board.hasHtml() && !board.hasRtf() && !board.hasUrl()) {
+                event.acceptTransferModes(TransferMode.NONE);
+            } else {
+                event.acceptTransferModes(TransferMode.ANY);
+            }
+
+        });
+        node.setOnDragDropped((event) -> {
+            Dragboard dragboard = event.getDragboard();
+            if (dragboard.hasString()) {
+                onAccept.accept(node, dragboard.getString());
+            } else if (dragboard.hasHtml()) {
+                onAccept.accept(node, dragboard.getHtml());
+            } else if (dragboard.hasRtf()) {
+                onAccept.accept(node, dragboard.getRtf());
+            } else if (dragboard.hasUrl()) {
+                onAccept.accept(node, dragboard.getUrl());
+            }
+
+        });
+    }
+
+    public static <T extends Node> void acceptFile(T node, BiConsumer<T, List<File>> onAccept) {
+        node.setOnDragOver((event) -> {
+            if (event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(TransferMode.ANY);
+            } else {
+                event.acceptTransferModes(TransferMode.NONE);
+            }
+
+        });
+        node.setOnDragDropped((event) -> {
+            Dragboard dragboard = event.getDragboard();
+            if (dragboard.hasFiles()) {
+                onAccept.accept(node, dragboard.getFiles());
+            }
+        });
+    }
+
+    public static <T extends Node> void accept(T node, Predicate<Dragboard> predicate, BiConsumer<T, Dragboard> onAccept) {
+        node.setOnDragOver((event) -> {
+            if (predicate.test(event.getDragboard())) {
+                event.acceptTransferModes(TransferMode.ANY);
+            } else {
+                event.acceptTransferModes(TransferMode.NONE);
+            }
+        });
+        node.setOnDragDropped((event) -> {
+            Dragboard dragboard = event.getDragboard();
+            onAccept.accept(node, dragboard);
+        });
+    }
+}
---
+++ core/src/main/java/com/tlcsdm/core/javafx/util/FileChooserUtil.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2023 unknowIfGuestInDream
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.core.javafx.util;
+
+import cn.hutool.core.io.FileUtil;
+import com.tlcsdm.core.javafx.helper.DropContentHelper;
+import com.tlcsdm.core.util.I18nUtils;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * @author: unknowIfGuestInDream
+ * @date: 2023/7/9 14:12
+ */
+public class FileChooserUtil {
+    public static final File HOME_DIRECTORY = FileSystemView.getFileSystemView().getHomeDirectory();
+
+    private FileChooserUtil() {
+    }
+
+    public static List<File> chooseFiles() {
+        return chooseFiles((FileChooser.ExtensionFilter[]) null);
+    }
+
+    public static List<File> chooseFiles(FileChooser.ExtensionFilter... extensionFilter) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(I18nUtils.get("core.util.fileChooser.title"));
+        fileChooser.setInitialDirectory(HOME_DIRECTORY);
+        if (extensionFilter != null) {
+            fileChooser.getExtensionFilters().addAll(extensionFilter);
+        }
+
+        return fileChooser.showOpenMultipleDialog(null);
+    }
+
+    public static File chooseFile() {
+        return chooseFile((FileChooser.ExtensionFilter[]) null);
+    }
+
+    public static File chooseFile(FileChooser.ExtensionFilter... extensionFilter) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(I18nUtils.get("core.util.fileChooser.title"));
+        fileChooser.setInitialDirectory(HOME_DIRECTORY);
+        if (extensionFilter != null) {
+            fileChooser.getExtensionFilters().addAll(extensionFilter);
+        }
+
+        return fileChooser.showOpenDialog(null);
+    }
+
+    public static File chooseSaveFile(FileChooser.ExtensionFilter... extensionFilter) {
+        return chooseSaveFile(null, extensionFilter);
+    }
+
+    public static File chooseSaveFile(String fileName) {
+        return chooseSaveFile(fileName, (FileChooser.ExtensionFilter[]) null);
+    }
+
+    public static File chooseSaveFile(String fileName, FileChooser.ExtensionFilter... extensionFilter) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(HOME_DIRECTORY);
+        if (fileName != null) {
+            fileChooser.setInitialFileName(fileName);
+        }
+
+        if (extensionFilter != null) {
+            fileChooser.getExtensionFilters().addAll(extensionFilter);
+        }
+
+        return fileChooser.showSaveDialog(null);
+    }
+
+    public static File chooseSaveCommonImageFile(String fileName) {
+        return chooseSaveFile(fileName, new FileChooser.ExtensionFilter("All Images", "*.*"), new FileChooser.ExtensionFilter("JPG", "*.jpg"), new FileChooser.ExtensionFilter("PNG", "*.png"), new FileChooser.ExtensionFilter("GIF", "*.gif"), new FileChooser.ExtensionFilter("JPEG", "*.jpeg"), new FileChooser.ExtensionFilter("BMP", "*.bmp"));
+    }
+
+    public static File chooseSaveImageFile(String fileName) {
+        return chooseSaveFile(fileName, new FileChooser.ExtensionFilter("All Images", "*.*"), new FileChooser.ExtensionFilter("JPG", "*.jpg"), new FileChooser.ExtensionFilter("PNG", "*.png"), new FileChooser.ExtensionFilter("gif", "*.gif"), new FileChooser.ExtensionFilter("jpeg", "*.jpeg"), new FileChooser.ExtensionFilter("bmp", "*.bmp"), new FileChooser.ExtensionFilter("ICO", "*.ico"), new FileChooser.ExtensionFilter("RGBE", "*.rgbe"));
+    }
+
+    public static File chooseDirectory() {
+        return chooseDirectory(null);
+    }
+
+    public static File chooseDirectory(File initialDirectory) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        if (initialDirectory != null) {
+            directoryChooser.setInitialDirectory(initialDirectory);
+        }
+
+        return directoryChooser.showDialog(null);
+    }
+
+    public static void setOnDrag(TextField textField, FileChooserUtil.FileType fileType) {
+        DropContentHelper.accept(textField, (dragboard) -> {
+            if (dragboard.hasFiles()) {
+                Stream<File> fileStream = dragboard.getFiles().stream();
+                Objects.requireNonNull(fileType);
+                if (fileStream.anyMatch(fileType::match)) {
+                    return true;
+                }
+            }
+            return false;
+        }, (__, dragboard) -> {
+            Stream<File> fileStream = dragboard.getFiles().stream();
+            Objects.requireNonNull(fileType);
+            textField.setText(fileStream.filter(fileType::match).map(File::getAbsolutePath).findFirst().orElse(""));
+        });
+    }
+
+    public static void setOnDragByOpenFile(TextInputControl textField) {
+        DropContentHelper.accept(textField, (dragboard) -> dragboard.hasFiles() && dragboard.getFiles().stream().anyMatch(File::isFile), (__, dragboard) -> {
+            textField.setText(dragboard.getFiles().stream().filter(File::isFile).map(FileUtil::readUtf8String).findFirst().orElse(""));
+        });
+    }
+
+    public static enum FileType {
+        FILE,
+        FOLDER;
+
+        private FileType() {
+        }
+
+        public boolean match(File file) {
+            return this == FILE && file.isFile() || this == FOLDER && file.isDirectory();
+        }
+    }
+}
