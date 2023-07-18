f810a63454b042d7202b80a64dafffab055420cc
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2023-07-18 16:48:23 +0800

    feat: fxml示例集成

--- qe/src/main/java/com/tlcsdm/qe/tools/FxmlDemo.java
+++ qe/src/main/java/com/tlcsdm/qe/tools/FxmlDemo.java
@@ -28,33 +28,48 @@
 package com.tlcsdm.qe.tools;
 
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.log.StaticLog;
 import com.tlcsdm.core.javafx.control.FxTextInput;
 import com.tlcsdm.core.javafx.util.Config;
 import com.tlcsdm.core.javafx.util.FxmlUtil;
 import com.tlcsdm.qe.QeSample;
 import com.tlcsdm.qe.util.I18nUtils;
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.SVGPath;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.StringReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * 测试用，测试fxml集成
@@ -64,7 +79,15 @@ import java.util.ResourceBundle;
 public class FxmlDemo extends QeSample implements Initializable {
 
     @FXML
-    private LineChart lineChart;
+    private LineChart<Integer, Double> lineChart;
+    @FXML
+    private NumberAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
+    @FXML
+    private Slider sliderLight;
+    @FXML
+    private ComboBox<Integer> cmbDimmingCurve;
     @FXML
     private Rectangle rectLighting;
     @FXML
@@ -72,9 +95,13 @@ public class FxmlDemo extends QeSample implements Initializable {
     @FXML
     private SVGPath svgBase;
     @FXML
-    private Label labelControl;
+    private Label lblControl;
+    @FXML
+    private Label lblActualLevel;
     @FXML
     private Button btnOff;
+    @FXML
+    private Button btnDimmingSet;
     @FXML
     private Button btnUp;
     @FXML
@@ -88,9 +115,13 @@ public class FxmlDemo extends QeSample implements Initializable {
     @FXML
     private Button btnMin;
     @FXML
+    private Button btnDirectLevel;
+    @FXML
+    private TextField txtDirectLevel;
+    @FXML
     private SplitPane splitPane;
     @FXML
-    private TreeView treeView;
+    private TreeView<String> treeView;
 
     @Override
     public boolean isVisible() {
@@ -143,28 +174,123 @@ public class FxmlDemo extends QeSample implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        initializeOption();
+        initializeUI();
     }
 
-    public SVGPath loadPathToMM(String pathName) {
+    public void initializeOption() {
+        svgLighting.opacityProperty().bind(Bindings.createDoubleBinding(() -> {
+            double level = sliderLight.getValue();
+            if (level < 26) {
+                return 0.1;
+            }
+            return level / 256;
+        }, sliderLight.valueProperty()));
+        lblActualLevel.textProperty().bind(Bindings.createStringBinding(() -> {
+            double level = daliDimmingCurve((int) sliderLight.getValue());
+            return String.format("%.2f", level) + "%";
+        }, sliderLight.valueProperty()));
+        Font font = Font.loadFont(getClass().getResourceAsStream("/com/tlcsdm/qe/fxml/digiface.ttf"),
+            lblActualLevel.getFont().getSize());
+        lblActualLevel.setFont(font);
+        cmbDimmingCurve.getItems().addAll(0, 1);
+        cmbDimmingCurve.getSelectionModel().select(0);
+        lineChart.getData().add(createSeries());
+
+        sliderLight.styleProperty().bind(Bindings.createStringBinding(() -> {
+            double percentage = (sliderLight.getValue() - sliderLight.getMin())
+                / (sliderLight.getMax() - sliderLight.getMin()) * 100.0;
+            return String.format("-slider-track-color: linear-gradient(to top, -slider-filled-track-color 0%%, "
+                + "-slider-filled-track-color %f%%, -fx-base %f%%, -fx-base 100%%);", percentage, percentage);
+        }, sliderLight.valueProperty(), sliderLight.minProperty(), sliderLight.maxProperty()));
+    }
+
+    public void initializeUI() {
+        initializeSvg();
+    }
+
+    @FXML
+    public void minLevelAction(ActionEvent event) {
+        sliderLight.setValue(sliderLight.getMin());
+    }
+
+    @FXML
+    public void maxLevelAction(ActionEvent event) {
+        sliderLight.setValue(sliderLight.getMax());
+    }
+
+    @FXML
+    public void upLevelAction(ActionEvent event) {
+        int level = (int) sliderLight.getValue();
+        if (level < sliderLight.getMax()) {
+            sliderLight.setValue(level + 1);
+        }
+    }
+
+    @FXML
+    public void downLevelAction(ActionEvent event) {
+        int level = (int) sliderLight.getValue();
+        if (level > sliderLight.getMin()) {
+            sliderLight.setValue(level - 1);
+        }
+    }
+
+    @FXML
+    public void directLevelAction(ActionEvent event) {
+        sliderLight.setValue(Integer.parseInt(txtDirectLevel.getText()));
+    }
+
+    @FXML
+    public void directLevelEnterAction(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            directLevelAction(null);
+        }
+    }
+
+    private void initializeSvg() {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        SVGPath path = new SVGPath();
         try {
             DocumentBuilder builder = factory.newDocumentBuilder();
-            //禁止DTD验证,防止网络阻塞
-            builder.setEntityResolver(
-                (publicId, systemId) -> new InputSource(new StringReader(""))
-            );
-            Document d = builder.parse(getClass().getResourceAsStream(pathName));
-            org.w3c.dom.Node node = d.getElementsByTagName("path").item(0);
-            String content = node.getAttributes().getNamedItem("d").getNodeValue();
-
-            path.setContent(content);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            Document doc = builder.parse(getClass().getResourceAsStream("/com/tlcsdm/qe/static/icon/lighting.svg"));
+            org.w3c.dom.Node nodeLighting = doc.getElementsByTagName("path").item(0);
+            org.w3c.dom.Node nodeBase = doc.getElementsByTagName("path").item(1);
+            parseXmlToSvg(svgLighting, nodeLighting);
+            parseXmlToSvg(svgBase, nodeBase);
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            StaticLog.error(e, "parse lighting.svg failed!");
         }
-        return path;
     }
 
+    private void parseXmlToSvg(SVGPath svg, org.w3c.dom.Node node) {
+        String content = node.getAttributes().getNamedItem("d").getNodeValue();
+        svg.setContent(content);
+        String colorString = node.getAttributes().getNamedItem("fill").getNodeValue();
+        svg.setFill(Color.web(colorString, 1));
+        if (node.getAttributes().getNamedItem("stroke") != null) {
+            String stroke = node.getAttributes().getNamedItem("stroke").getNodeValue();
+            svg.setStroke(Color.web(stroke, 1));
+        }
+        if (node.getAttributes().getNamedItem("stroke-width") != null) {
+            String strokeWidth = node.getAttributes().getNamedItem("stroke-width").getNodeValue();
+            svg.setStrokeWidth(Double.parseDouble(strokeWidth));
+        }
+    }
+
+    private Series<Integer, Double> createSeries() {
+        Series<Integer, Double> series = new Series<>();
+        for (int i = 0; i < 256; i++) {
+            series.getData().add(new Data<>(i, daliDimmingCurve(i)));
+        }
+        return series;
+    }
+
+    private double daliDimmingCurve(int x) {
+        if (x == 255) {
+            return 100;
+        } else if (x == 0) {
+            return 0;
+        } else {
+            return Math.pow(10, (double) (x - 1) * 3 / 253 - 1);
+        }
+    }
 }
