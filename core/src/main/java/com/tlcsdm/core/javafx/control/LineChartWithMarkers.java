02ce226ce051e6b9b9e6f0611450d5d7c32d812e
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2023-07-27 22:20:01 +0800

    test(qe): fxml demo

--- core/src/main/java/com/tlcsdm/core/javafx/control/LineChartWithMarkers.java
+++ core/src/main/java/com/tlcsdm/core/javafx/control/LineChartWithMarkers.java
@@ -1,30 +1,3 @@
-/*
- * Copyright (c) 2023 unknowIfGuestInDream
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
- * names of its contributors may be used to endorse or promote products
- * derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package com.tlcsdm.core.javafx.control;
 
 import javafx.beans.InvalidationListener;
@@ -32,14 +5,13 @@ import javafx.beans.NamedArg;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.shape.Line;
 
 /**
- * 带有辅助线的LineChart
- *
- * @author unknowIfGuestInDream
+ * LineChart that can add horizontal and vertical axis auxiliary lines
  */
 public class LineChartWithMarkers<X, Y> extends LineChart<X, Y> {
 
@@ -112,6 +84,10 @@ public class LineChartWithMarkers<X, Y> extends LineChart<X, Y> {
         verticalMarkers.clear();
     }
 
+    public ObservableList<Node> plotChildren() {
+        return getPlotChildren();
+    }
+
     @Override
     protected void layoutPlotChildren() {
         horizontalMarkers.forEach(this::drawHorizontalMarker);
@@ -123,16 +99,14 @@ public class LineChartWithMarkers<X, Y> extends LineChart<X, Y> {
         Line line = (Line) horizontalMarker.getNode();
         line.setStartX(0);
         line.setEndX(getBoundsInLocal().getWidth());
-        // 0.5 for crispness
-        line.setStartY(getYAxis().getDisplayPosition(horizontalMarker.getYValue()) + 0.5);
+        line.setStartY(getYAxis().getDisplayPosition(horizontalMarker.getYValue()) + 0.5); // 0.5 for crispness
         line.setEndY(line.getStartY());
         line.toFront();
     }
 
     private void drawVerticalMarker(Data<X, Y> verticalMarker) {
         Line line = (Line) verticalMarker.getNode();
-        // 0.5 for crispness
-        line.setStartX(getXAxis().getDisplayPosition(verticalMarker.getXValue()) + 0.5);
+        line.setStartX(getXAxis().getDisplayPosition(verticalMarker.getXValue()) + 0.5); // 0.5 for crispness
         line.setEndX(line.getStartX());
         line.setStartY(0d);
         line.setEndY(getBoundsInLocal().getHeight());
