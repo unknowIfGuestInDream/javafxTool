e98c978bb42a34045e8615e5932beeeabc2a03f4
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2023-09-03 17:34:55 +0800

    build(smc): Prepare for 1.0.7

--- smc/src/main/java/com/tlcsdm/smc/util/SmcConstant.java
+++ smc/src/main/java/com/tlcsdm/smc/util/SmcConstant.java
@@ -88,11 +88,11 @@ public class SmcConstant {
     /**
      * 默认项目版本号
      */
-    public static final String PROJECT_VERSION = "1.0.6";
+    public static final String PROJECT_VERSION = "1.0.7";
     /**
      * 项目构建日期
      */
-    public static final String PROJECT_BUILD_DAY = "2023-08-29";
+    public static final String PROJECT_BUILD_DAY = "2023-09-03";
     /**
      * 检查更新所用的api
      */
