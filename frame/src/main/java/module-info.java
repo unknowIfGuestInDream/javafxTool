8d4eb83f5385621cfb637b3974bc7f0ce45e7333
梦里不知身是客 <liang.tang.cx@gmail.com>
2023-09-23 20:41:26 +0800

    feat(frame): 新增菜单缓存 (#748)
    * feat(frame): 新增菜单缓存
    
    * build: 依赖配置修改
    
    * chore: 依赖更新
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    * docs: 补充注释
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    * feat: 缓存工厂实现
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    * feat: 对UI进行缓存
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    * fix: 指摘对应
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    ---------
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

--- frame/src/main/java/module-info.java
+++ frame/src/main/java/module-info.java
@@ -8,11 +8,13 @@ module com.tlcsdm.frame {
     requires cn.hutool.core;
     requires cn.hutool.log;
     requires cn.hutool.crypto;
+    requires static com.github.benmanes.caffeine;
 
     exports com.tlcsdm.frame;
     exports com.tlcsdm.frame.model;
     exports com.tlcsdm.frame.service;
     exports com.tlcsdm.frame.event;
+    exports com.tlcsdm.frame.cache;
 
     uses com.tlcsdm.frame.service.FXSamplerProject;
     uses com.tlcsdm.frame.service.MenubarConfigration;
