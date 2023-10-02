2977091996a11f7c622cda453118af6593ff4c8d
梦里不知身是客 <liang.tang.cx@gmail.com>
2023-10-02 17:28:25 +0800

    feat(cg): 初始化CG (#804)
    * feat(cg): 初始化CG
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    * feat(cg): 初始化CG
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    ---------
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

---
+++ cg/src/main/java/com/tlcsdm/cg/CgSample.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2019, 2023 unknowIfGuestInDream
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.cg;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.log.StaticLog;
+import com.tlcsdm.cg.util.CgConstant;
+import com.tlcsdm.frame.SampleBase;
+
+import java.io.InputStream;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+/**
+ * @author unknowIfGuestInDream
+ */
+public abstract class CgSample extends SampleBase {
+    public static final ProjectInfo PROJECT_INFO = new ProjectInfo();
+
+    @Override
+    public String getProjectName() {
+        return CgConstant.PROJECT_NAME;
+    }
+
+    @Override
+    public String getProjectVersion() {
+        return PROJECT_INFO.getVersion();
+    }
+
+    public static class ProjectInfo {
+
+        private String version;
+        private String date;
+
+        public ProjectInfo() {
+
+            try {
+                InputStream s = CgSampler.class.getModule().getResourceAsStream("META-INF/MANIFEST.MF");
+                Manifest manifest = new Manifest(s);
+                Attributes attr = manifest.getMainAttributes();
+                //CgConstant变量是为了打包exe时使用
+                version = StrUtil.blankToDefault(attr.getValue("Implementation-Version"), CgConstant.PROJECT_VERSION);
+                date = StrUtil.blankToDefault(attr.getValue("Build-Day"), CgConstant.PROJECT_BUILD_DAY);
+            } catch (Throwable e) {
+                version = CgConstant.PROJECT_VERSION;
+                date = CgConstant.PROJECT_BUILD_DAY;
+                StaticLog.error("Fail to get MANIFEST.MF.");
+            }
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        public String getDate() {
+            return date;
+        }
+    }
+
+}
