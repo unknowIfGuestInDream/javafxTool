60ae592cd94a5bbbc38411c761d3cbd05dc063b6
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2023-10-15 03:10:41 +0800

    test: Test for vosk
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

--- core/src/test/java/com/tlcsdm/core/util/VoskTest.java
+++ core/src/test/java/com/tlcsdm/core/util/VoskTest.java
@@ -26,7 +26,7 @@ public class VoskTest {
     @Test
     void decoder() throws IOException, UnsupportedAudioFileException {
         LibVosk.setLogLevel(LogLevel.DEBUG);
-        try (Model model = new Model("E:\\javaWorkSpace\\javafxTool\\runtime\\modal\\vosk-model-small-cn");
+        try (Model model = new Model("D:\\minIO\\Data\\modal\\vosk-model-cn-0.22");
              InputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream
                  (new FileInputStream("E:\\testPlace\\vosk\\test.wav")));
              Recognizer recognizer = new Recognizer(model, 16000)) {
@@ -35,14 +35,17 @@ public class VoskTest {
             byte[] b = new byte[4096];
             while ((nbytes = ais.read(b)) >= 0) {
                 if (recognizer.acceptWaveForm(b, nbytes)) {
-                    System.out.println(recognizer.getResult());
+                    //System.out.println(recognizer.getResult());
+                    if (!recognizer.getResult().isEmpty()) {
+                        FileUtil.appendUtf8String(recognizer.getResult() + "\n", "E:\\testPlace\\vosk\\result.txt");
+                    }
                 } else {
-                    System.out.println(recognizer.getPartialResult());
+                    // System.out.println(recognizer.getPartialResult());
                 }
             }
             //Console.log(recognizer.getFinalResult());
             String result = recognizer.getFinalResult();
-            FileUtil.writeUtf8String(result, "E:\\testPlace\\vosk\\result.txt");
+            FileUtil.appendUtf8String(result, "E:\\testPlace\\vosk\\result.txt");
         }
     }
 }
