de737b4803099364c6531fd0ce60f5870dee3b29
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2023-10-22 13:45:30 +0800

    feat: vosk工具类完善
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -38,10 +38,12 @@ module com.tlcsdm.core {
     requires static com.dlsc.preferencesfx;
     requires static yuicompressor;
     requires static jave.core;
+    requires static vosk;
 
     opens com.tlcsdm.core.javafx.controller to javafx.fxml;
     opens com.tlcsdm.core.javafx.view to javafx.fxml;
     opens com.tlcsdm.core.util to org.junit.platform.engine, org.junit.jupiter.api;
+    opens com.tlcsdm.core.modal;
 
     exports com.tlcsdm.core.javafx;
     exports com.tlcsdm.core.javafx.control;
--- core/src/test/java/com/tlcsdm/core/util/VoskTest.java
+++ core/src/test/java/com/tlcsdm/core/util/VoskTest.java
@@ -2,17 +2,9 @@ package com.tlcsdm.core.util;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
-import org.vosk.LibVosk;
-import org.vosk.LogLevel;
-import org.vosk.Model;
-import org.vosk.Recognizer;
 
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.File;
+import java.util.List;
 
 /**
  * Vosk 语音转换测试.
@@ -23,24 +15,10 @@ import java.io.InputStream;
 class VoskTest {
 
     @Test
-    void decoder() throws IOException, UnsupportedAudioFileException {
-        LibVosk.setLogLevel(LogLevel.INFO);
-        try (Model model = new Model("D:\\minIO\\Data\\modal\\vosk-model-cn-0.22");
-             InputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream
-                 (new FileInputStream("E:\\testPlace\\vosk\\test.wav")));
-             Recognizer recognizer = new Recognizer(model, 16000)) {
-
-            int nbytes;
-            byte[] b = new byte[4096];
-            while ((nbytes = ais.read(b)) >= 0) {
-                if (recognizer.acceptWaveForm(b, nbytes)) {
-                    System.out.println(recognizer.getResult());
-                } else {
-                    System.out.println(recognizer.getPartialResult());
-                }
-            }
-            String result = recognizer.getFinalResult();
-            System.out.println(result);
-        }
+    void decoder() {
+        List<String> list = VoskUtil.decoder("E:\\testPlace\\vosk\\test.wav",
+            CoreUtil.getRootPath() + File.separator + "runtime" + File.separator + "modal" + File.separator
+                + "vosk-model-cn");
+        list.forEach(System.out::println);
     }
 }
