dca5e5ac440dcefbb811b3afd030ad70e0bbc69b
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2023-11-07 00:15:03 +0800

    feat: Add testfx
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

---
+++ qe/src/test/java/com/tlcsdm/qe/tools/ClickableButtonTest_JUnit5AssertJ.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2023 unknowIfGuestInDream.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.qe.tools;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
+import org.testfx.framework.junit5.ApplicationExtension;
+
+@ExtendWith(ApplicationExtension.class)
+class ClickableButtonTest_JUnit5AssertJ {
+
+    private Button button;
+
+    /**
+     * Will be called with {@code @Before} semantics, i. e. before each test method.
+     *
+     * @param stage - Will be injected by the test runner.
+     */
+    @BeforeEach
+    private void start(Stage stage) {
+        button = new Button("click me!");
+        button.setId("myButton");
+        button.setOnAction(actionEvent -> button.setText("clicked!"));
+        stage.setScene(new Scene(new StackPane(button), 100, 100));
+        stage.show();
+    }
+
+    /**
+     * @param robot - Will be injected by the test runner.
+     */
+    @Test
+    void should_contain_button_with_text(FxRobot robot) {
+        Assertions.assertThat(button).hasText("click me!");
+        // or (lookup by css id):
+        Assertions.assertThat(robot.lookup("#myButton").queryAs(Button.class)).hasText("click me!");
+        // or (lookup by css class):
+        Assertions.assertThat(robot.lookup(".button").queryAs(Button.class)).hasText("click me!");
+        // or (query specific type):
+        Assertions.assertThat(robot.lookup(".button").queryButton()).hasText("click me!");
+    }
+
+    /**
+     * @param robot - Will be injected by the test runner.
+     */
+    @Test
+    void when_button_is_clicked_text_changes(FxRobot robot) {
+        // when:
+        robot.clickOn(".button");
+
+        // then:
+        Assertions.assertThat(button).hasText("clicked!");
+        // or (lookup by css id):
+        Assertions.assertThat(robot.lookup("#myButton").queryAs(Button.class)).hasText("clicked!");
+        // or (lookup by css class):
+        Assertions.assertThat(robot.lookup(".button").queryAs(Button.class)).hasText("clicked!");
+        // or (query specific type)
+        Assertions.assertThat(robot.lookup(".button").queryButton()).hasText("clicked!");
+    }
+}
