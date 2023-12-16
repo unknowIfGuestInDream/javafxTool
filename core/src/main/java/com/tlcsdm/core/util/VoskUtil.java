f12db642c5bdccfd2206cbc117b6ce0312137bea
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2023-12-16 20:48:34 +0800

    fix: VoskUtil error
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

--- core/src/main/java/com/tlcsdm/core/util/VoskUtil.java
+++ core/src/main/java/com/tlcsdm/core/util/VoskUtil.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2023 unknowIfGuestInDream.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.tlcsdm.core.util;
 
 import cn.hutool.core.io.FileUtil;
@@ -20,11 +47,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * vosk工具类.
- *
- * @author unknowIfGuestInDream
- */
 public class VoskUtil {
 
     /**
@@ -35,6 +57,7 @@ public class VoskUtil {
             throw new CoreException("Only WAV audio files are supported.");
         }
         List<String> list = new ArrayList<>();
+        System.setProperty("jna.encoding", CoreConstant.ENCODING_UTF_8);
         LibVosk.setLogLevel(LogLevel.INFO);
         try (Model model = new Model(modalPath);
              InputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream
