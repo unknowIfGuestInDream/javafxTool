04c230a0ce1b79eaabc3c8e9016cfd4deab9d427
IfGuestInDream <ifGuestInDream@163.com>
2023-12-23 17:01:48 +0800

    style: Format code
    Signed-off-by: IfGuestInDream <ifGuestInDream@163.com>

--- core/src/main/java/com/tlcsdm/core/javafx/bind/ComboBoxBaseEmptyBinding.java
+++ core/src/main/java/com/tlcsdm/core/javafx/bind/ComboBoxBaseEmptyBinding.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 unknowIfGuestInDream
+ * Copyright (c) 2023 unknowIfGuestInDream.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
