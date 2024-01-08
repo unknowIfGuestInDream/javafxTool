19aba74ebf1ddbe156ac83b918c53b83ec40787a
梦里不知身是客 <liang.tang.cx@gmail.com>
2024-01-08 21:28:41 +0800

    feat: Add WeakConcurrent (#1171)
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

---
+++ core/src/test/java/com/tlcsdm/core/concurrent/DetachedThreadLocalTest.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2024 unknowIfGuestInDream.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.core.concurrent;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author unknowIfGuestInDream
+ */
+public class DetachedThreadLocalTest {
+
+    @Test
+    public void testLocalExpunction() throws Exception {
+        final DetachedThreadLocal<Object> threadLocal = new DetachedThreadLocal<>(
+            DetachedThreadLocal.Cleaner.INLINE);
+        Assertions.assertNull(threadLocal.getBackingMap().getCleanerThread());
+        new ThreadLocalTestCase(threadLocal) {
+            @Override
+            protected void triggerClean() {
+                threadLocal.map.expungeStaleEntries();
+            }
+        }.doTest();
+    }
+
+    @Test
+    public void testExternalThread() throws Exception {
+        DetachedThreadLocal<Object> threadLocal = new DetachedThreadLocal<>(DetachedThreadLocal.Cleaner.MANUAL);
+        Assertions.assertNull(threadLocal.getBackingMap().getCleanerThread());
+        Thread thread = new Thread(threadLocal);
+        thread.start();
+        new ThreadLocalTestCase(threadLocal).doTest();
+        thread.interrupt();
+        Thread.sleep(200L);
+        Assertions.assertFalse(thread.isAlive());
+    }
+
+    @Test
+    public void testInternalThread() throws Exception {
+        DetachedThreadLocal<Object> threadLocal = new DetachedThreadLocal<>(DetachedThreadLocal.Cleaner.THREAD);
+        Assertions.assertNotNull(threadLocal.getBackingMap().getCleanerThread());
+        new ThreadLocalTestCase(threadLocal).doTest();
+        threadLocal.getBackingMap().getCleanerThread().interrupt();
+        Thread.sleep(200L);
+        Assertions.assertFalse(threadLocal.getBackingMap().getCleanerThread().isAlive());
+    }
+
+    private static class ThreadLocalTestCase {
+
+        private final DetachedThreadLocal<Object> threadLocal;
+
+        public ThreadLocalTestCase(DetachedThreadLocal<Object> threadLocal) {
+            this.threadLocal = threadLocal;
+        }
+
+        void doTest() throws Exception {
+            int size = 100;
+            List<Thread> threads = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+                Thread thread = new Thread(new ThreadLocalInteraction(threadLocal));
+                threads.add(thread);
+                thread.start();
+            }
+            for (Thread thread : threads) {
+                thread.join();
+            }
+            Assertions.assertEquals(threadLocal.map.target.size(), size);
+            threads.clear();
+            System.gc();
+            Thread.sleep(1500L);
+            System.gc();
+            Thread.sleep(1500L);
+            triggerClean();
+            Assertions.assertEquals(threadLocal.map.target.size(), 0);
+        }
+
+        void triggerClean() {
+        }
+    }
+
+    private static class ThreadLocalInteraction implements Runnable {
+
+        private final DetachedThreadLocal<Object> threadLocal;
+
+        ThreadLocalInteraction(DetachedThreadLocal<Object> threadLocal) {
+            this.threadLocal = threadLocal;
+        }
+
+        @Override
+        public void run() {
+            Object value = new Object();
+            threadLocal.set(value);
+            Assertions.assertEquals(threadLocal.get(), value);
+        }
+    }
+}
---
+++ core/src/test/java/com/tlcsdm/core/concurrent/WeakConcurrentSetTest.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2024 unknowIfGuestInDream.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.core.concurrent;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author unknowIfGuestInDream
+ */
+public class WeakConcurrentSetTest {
+
+    @Test
+    public void testLocalExpunction() throws Exception {
+        final WeakConcurrentSet<Object> set = new WeakConcurrentSet<Object>(WeakConcurrentSet.Cleaner.INLINE);
+        Assertions.assertNull(set.getCleanerThread());
+        new SetTestCase(set) {
+            @Override
+            protected void triggerClean() {
+                set.target.expungeStaleEntries();
+            }
+        }.doTest();
+    }
+
+    @Test
+    public void testExternalThread() throws Exception {
+        WeakConcurrentSet<Object> set = new WeakConcurrentSet<Object>(WeakConcurrentSet.Cleaner.MANUAL);
+        Assertions.assertNull(set.getCleanerThread());
+        Thread thread = new Thread(set);
+        thread.start();
+        new SetTestCase(set).doTest();
+        thread.interrupt();
+        Thread.sleep(200L);
+        Assertions.assertFalse(thread.isAlive());
+    }
+
+    @Test
+    public void testInternalThread() throws Exception {
+        WeakConcurrentSet<Object> set = new WeakConcurrentSet<Object>(WeakConcurrentSet.Cleaner.THREAD);
+        Assertions.assertNotNull(set.getCleanerThread());
+        new SetTestCase(set).doTest();
+        set.getCleanerThread().interrupt();
+        Thread.sleep(200L);
+        Assertions.assertFalse(set.getCleanerThread().isAlive());
+    }
+
+    private static class SetTestCase {
+
+        private final WeakConcurrentSet<Object> set;
+
+        public SetTestCase(WeakConcurrentSet<Object> set) {
+            this.set = set;
+        }
+
+        void doTest() throws Exception {
+            Object value1 = new Object(), value2 = new Object(), value3 = new Object(), value4 = new Object();
+            set.add(value1);
+            set.add(value2);
+            set.add(value3);
+            set.add(value4);
+            Assertions.assertTrue(set.contains(value1));
+            Assertions.assertTrue(set.contains(value2));
+            Assertions.assertTrue(set.contains(value3));
+            Assertions.assertTrue(set.contains(value4));
+            Set<Object> values = new HashSet<Object>(Arrays.asList(value1, value2, value3, value4));
+            for (Object value : set) {
+                Assertions.assertTrue(values.remove(value));
+            }
+            Assertions.assertTrue(values.isEmpty());
+            // Make eligible for GC
+            value1 = value2 = null;
+            System.gc();
+            Thread.sleep(200L);
+            triggerClean();
+            Assertions.assertTrue(set.contains(value3));
+            Assertions.assertTrue(set.contains(value4));
+            Assertions.assertEquals(set.approximateSize(), 2);
+            Assertions.assertEquals(set.target.target.size(), 2);
+            Assertions.assertTrue(set.remove(value3));
+            Assertions.assertFalse(set.contains(value3));
+            Assertions.assertTrue(set.contains(value4));
+            Assertions.assertEquals(set.approximateSize(), 1);
+            Assertions.assertEquals(set.target.target.size(), 1);
+            set.clear();
+            Assertions.assertFalse(set.contains(value3));
+            Assertions.assertFalse(set.contains(value4));
+            Assertions.assertEquals(set.approximateSize(), 0);
+            Assertions.assertEquals(set.target.target.size(), 0);
+            Assertions.assertFalse(set.iterator().hasNext());
+        }
+
+        protected void triggerClean() {
+        }
+    }
+
+    @Test
+    public void testSetContract() {
+        final WeakConcurrentSet<Object> set = new WeakConcurrentSet<Object>(WeakConcurrentSet.Cleaner.INLINE);
+        Object obj = new Object();
+        Assertions.assertFalse(set.contains(obj));
+        Assertions.assertFalse(set.remove(obj));
+        Assertions.assertTrue(set.add(obj));
+        Assertions.assertFalse(set.add(obj));
+        Assertions.assertTrue(set.contains(obj));
+        Assertions.assertTrue(set.remove(obj));
+        Assertions.assertFalse(set.contains(obj));
+        Assertions.assertFalse(set.remove(obj));
+    }
+}
