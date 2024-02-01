c148ad7eef0282ba2ebab44246d9a690f2581696
梦里不知身是客 <liang.tang.cx@gmail.com>
2024-02-01 22:30:57 +0800

    feat: Improve RangeSlider (#1236)
    * feat: Improve RangeSlider
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    * feat: Improve RangeSlider
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    * feat: Improve RangeSlider
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    * feat: Improve RangeSlider
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    * feat: Improve RangeSlider
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>
    
    ---------
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

--- core/src/main/java/com/tlcsdm/core/javafx/control/DecorationTextfield.java
+++ core/src/main/java/com/tlcsdm/core/javafx/control/DecorationTextfield.java
@@ -35,7 +35,29 @@ import javafx.scene.image.ImageView;
 import java.util.Objects;
 
 /**
+ * The DecorationTextfield control is simply a JavaFX {@link javafx.scene.control.TextField} control
+ * with support for decoration.It supports the display of error, warning and
+ * info images.
+ *
+ * <p>
+ * The image is on the right side by default and is not displayed initially.
+ * When {@link DecorationTextfield#setDecoration(Severity, String)} is called
+ * and the parameter is {@link Severity#ERROR}, {@link Severity#WARNING} or
+ * {@link Severity#INFO}, the image is displayed.
+ *
+ * <h3>Code Samples</h3>
+ * <p>
+ * If you want the image to be displayed outside the text box, you can use
+ * {@link DecorationTextfield#setOffsetX} to achieve it.
+ *
+ * <pre>{@code
+ * final DecorationTextfield text = new DecorationTextfield();
+ * text.setOffsetX(25);
+ * }</pre>
+ *
  * @author unknowIfGuestInDream
+ * @see CustomTextField
+ * @see javafx.scene.control.TextField
  */
 public class DecorationTextfield extends CustomTextField {
     private ImageView decoration;
@@ -60,26 +82,35 @@ public class DecorationTextfield extends CustomTextField {
         decoration.setFocusTraversable(false);
         decoration.setFitHeight(16);
         decoration.setPreserveRatio(true);
-        decoration.setStyle("-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);");
-        decoration.setCursor(Cursor.HAND);
+        decoration.setStyle("-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.6), 10, 0, 0, 0);");
         tooltip = new Tooltip();
         tooltip.setOpacity(0.9);
         tooltip.setAutoFix(true);
-        // setOffsetX(25);
-        Tooltip.install(decoration, tooltip);
+        tooltip.setWrapText(true);
+        tooltip.textProperty().addListener(o -> {
+            boolean enableTooltip = decoration.getProperties().containsKey("javafx.scene.control.Tooltip");
+            if (tooltip.getText() == null || tooltip.getText().isEmpty()) {
+                if (enableTooltip) {
+                    Tooltip.uninstall(decoration, tooltip);
+                    decoration.setCursor(Cursor.DEFAULT);
+                }
+            } else {
+                if (!enableTooltip) {
+                    Tooltip.install(decoration, tooltip);
+                    decoration.setCursor(Cursor.HAND);
+                }
+            }
+        });
         setRight(decoration);
     }
 
-    /**
-     * Defines severity of messages.
-     */
-    public enum Severity {
-        OK, INFO, WARNING, ERROR
+    public void setDecoration(Severity severity, String message) {
+        tooltip.setText(message);
+        decoration.setImage(getGraphicBySeverity(severity));
     }
 
-    public void setDecoration(Severity severity, String message) {
-        decoration.setImage(getGraphicBySeverity(severity));
-        tooltip.setText(message);
+    public void setDecoration(Severity severity) {
+        setDecoration(severity, null);
     }
 
     private Image getGraphicBySeverity(Severity severity) {
