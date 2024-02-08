eaaa39307d9cee59fc4f60e494e4b968d84ff440
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2024-02-09 00:09:53 +0800

    fix: Build result error
    Close #1283
    
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

--- jenkins/win/smc/smcTool.bat
+++ jenkins/win/smc/smcTool.bat
@@ -1,7 +1,18 @@
 @echo off
+set check_flag=true
 set java_path=.\jre\bin\javaw.exe
 if exist .\jre\bin\javaw.exe (
+  %java_path% -version
 ) else (
+if "true" == "%check_flag%" (
+  java -version
+)
+if not %errorlevel% == 0 (
+  echo Can not run java,check it.
+  echo %errorlevel%
+  pause
+  goto END
+)
 set java_path=javaw
 )
 :START
