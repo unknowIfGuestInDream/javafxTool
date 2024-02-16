7270bd94ecc0f551731527cff2bb560643eb4a05
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2024-02-16 11:02:37 +0800

    test: Add common-compress
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

--- core/src/test/java/com/tlcsdm/core/pmd/cpp/CPPTokenizerContinuationTest.java
+++ core/src/test/java/com/tlcsdm/core/pmd/cpp/CPPTokenizerContinuationTest.java
@@ -46,8 +46,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class CPPTokenizerContinuationTest {
 
