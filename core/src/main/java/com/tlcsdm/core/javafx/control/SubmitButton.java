2bba90ce10cee58a618267307b23ebd84914a828
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2024-03-03 01:08:50 +0800

    feat: Add SubmitButton
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

--- core/src/main/java/com/tlcsdm/core/javafx/control/SubmitButton.java
+++ core/src/main/java/com/tlcsdm/core/javafx/control/SubmitButton.java
@@ -106,14 +106,15 @@ public class SubmitButton extends Region {
     private final String checkPath = "M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z";
     private final String failPath = "M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z";
 
-    // ******************** Constructors **************************************
+    private final String defColor = "#00ca94";
+
     public SubmitButton() {
         this("Submit");
     }
 
-    public SubmitButton(final String TEXT) {
+    public SubmitButton(final String text) {
         keepAspect = true;
-        color = new ObjectPropertyBase<>(Color.web("#00ca94")) {
+        color = new ObjectPropertyBase<>(Color.web(defColor)) {
             @Override
             protected void invalidated() {
                 super.invalidated();
@@ -135,7 +136,7 @@ public class SubmitButton extends Region {
                 return "color";
             }
         };
-        frameColor = new ObjectPropertyBase<>(Color.web("#00ca94")) {
+        frameColor = new ObjectPropertyBase<>(Color.web(defColor)) {
             @Override
             public Object getBean() {
                 return SubmitButton.this;
@@ -157,7 +158,7 @@ public class SubmitButton extends Region {
                 return "buttonColor";
             }
         };
-        textColor = new ObjectPropertyBase<>(Color.web("#00ca94")) {
+        textColor = new ObjectPropertyBase<>(Color.web(defColor)) {
             @Override
             public Object getBean() {
                 return SubmitButton.this;
@@ -258,7 +259,7 @@ public class SubmitButton extends Region {
         borderWidth = 0d;
         timeline = new Timeline();
         init();
-        initGraphics(TEXT);
+        initGraphics(text);
         registerListeners();
     }
 
@@ -281,7 +282,7 @@ public class SubmitButton extends Region {
         }
     }
 
-    private void initGraphics(final String TEXT) {
+    private void initGraphics(final String txt) {
         frame = new Rectangle(PREFERRED_WIDTH, PREFERRED_HEIGHT, frameColor.get());
         frame.setArcWidth(PREFERRED_HEIGHT);
         frame.setArcHeight(PREFERRED_HEIGHT);
@@ -291,7 +292,7 @@ public class SubmitButton extends Region {
         buttonArea.setArcWidth(PREFERRED_HEIGHT);
         buttonArea.setArcHeight(PREFERRED_HEIGHT);
 
-        text = new Text(TEXT);
+        text = new Text(txt);
         text.setFont(Font.font(48));
         text.setFill(textColor.get());
         text.setMouseTransparent(true);
@@ -347,8 +348,8 @@ public class SubmitButton extends Region {
         return color.get();
     }
 
-    public void setColor(final Color COLOR) {
-        color.set(COLOR);
+    public void setColor(final Color col) {
+        color.set(col);
     }
 
     public ObjectProperty<Color> colorProperty() {
