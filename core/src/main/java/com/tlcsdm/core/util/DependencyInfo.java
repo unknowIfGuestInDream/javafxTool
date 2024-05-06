eefdb6d25eac6987e978c152c5f3e30ade662b59
梦里不知身是客 <liang.tang.cx@gmail.com>
2024-05-06 09:36:21 +0800

    docs: Add doc

--- core/src/main/java/com/tlcsdm/core/util/DependencyInfo.java
+++ core/src/main/java/com/tlcsdm/core/util/DependencyInfo.java
@@ -76,7 +76,7 @@ public class DependencyInfo {
                 "https://commons.apache.org/proper/commons-email/", "Apache License, Version 2.0",
                 "https://www.apache.org/licenses/LICENSE-2.0"),
 
-            new Dependency("org.apache.commons", "commons-csv", "1.10.0", false,
+            new Dependency("org.apache.commons", "commons-csv", "1.11.0", false,
                 "https://commons.apache.org/proper/commons-csv/", "Apache License, Version 2.0",
                 "https://www.apache.org/licenses/LICENSE-2.0"),
 
