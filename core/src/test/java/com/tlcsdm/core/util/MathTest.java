5507ec2b9e7ff95045261bbda8fdea36188c18f1
unknowIfGuestInDream <liang.tang.cx@gmail.com>
2024-06-14 00:02:01 +0800

    test: Add math test
    Signed-off-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

---
+++ core/src/test/java/com/tlcsdm/core/util/MathTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2024 unknowIfGuestInDream.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.core.util;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author unknowIfGuestInDream
+ */
+public class MathTest {
+
+    /**
+     * 平方根
+     */
+    @Test
+    void sqrt() {
+        Assertions.assertEquals(2, Math.sqrt(4));
+    }
+
+    /**
+     * 幂运算
+     */
+    @Test
+    void pow() {
+        Assertions.assertEquals(16, Math.pow(2, 4));
+        Assertions.assertEquals(1.3703509847201236, Math.pow(2, 1 / 2.2));
+    }
+
+    /**
+     * 矩阵计算
+     */
+    @Test
+    void Matrices() {
+        double[][] firstMatrix = {
+            new double[]{1d, 5d},
+            new double[]{2d, 3d},
+            new double[]{1d, 7d}
+        };
+        double[][] secondMatrix = {
+            new double[]{1d, 2d, 3d, 7d},
+            new double[]{5d, 2d, 8d, 1d}
+        };
+        // 预期结果
+        double[][] expected = {
+            new double[]{26d, 12d, 43d, 12d},
+            new double[]{17d, 10d, 30d, 17d},
+            new double[]{36d, 16d, 59d, 14d}
+        };
+        RealMatrix matrix1 = new Array2DRowRealMatrix(firstMatrix);
+        RealMatrix matrix2 = new Array2DRowRealMatrix(secondMatrix);
+        RealMatrix matrix3 = new Array2DRowRealMatrix(expected);
+        RealMatrix actual = matrix1.multiply(matrix2);
+        Assertions.assertEquals(actual, matrix3);
+    }
+}
