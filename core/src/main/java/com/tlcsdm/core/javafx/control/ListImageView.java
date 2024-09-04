434c469996017dffe183a3f8eb506da910147fbe
梦里不知身是客 <liang.tang.cx@gmail.com>
2024-09-04 18:01:16 +0800

    feat: Add ListImageView (#1696)
    * feat: Add ListImageView
    
    * feat: Add ListImageView
    
    ---------
    
    Co-authored-by: unknowIfGuestInDream <liang.tang.cx@gmail.com>

---
+++ core/src/main/java/com/tlcsdm/core/javafx/control/ListImageView.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2024 unknowIfGuestInDream.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of unknowIfGuestInDream, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UNKNOWIFGUESTINDREAM BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tlcsdm.core.javafx.control;
+
+import com.tlcsdm.core.javafx.control.skin.ListImageViewSkin;
+import javafx.beans.DefaultProperty;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Control;
+import javafx.scene.control.Skin;
+
+@DefaultProperty("items")
+public class ListImageView extends Control {
+
+    private ListProperty<Photo> items;
+
+    public ListImageView() {
+        this(FXCollections.observableArrayList());
+    }
+
+    public ListImageView(ObservableList<Photo> listOfPhotos) {
+        super();
+        getStyleClass().add("list-image-view");
+        items = new SimpleListProperty<>(this, "items");
+        items.set(listOfPhotos);
+    }
+
+    public final ListProperty<Photo> itemsProperty() {
+        return items;
+    }
+
+    public void setItems(ObservableList<Photo> photoList) {
+        this.items.set(photoList);
+    }
+
+    public ObservableList<Photo> getItems() {
+        return items.get();
+    }
+
+    @Override
+    protected Skin<?> createDefaultSkin() {
+        return new ListImageViewSkin(this);
+    }
+
+    public record Photo(String location, String title) {
+    }
+
+}
