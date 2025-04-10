30212ec8f5982756f6ccc8f8cc3777cd273e60ce
梦里不知身是客 <liang.tang.cx@gmail.com>
2025-04-10 22:38:08 +0800

    docs: Add doc

--- core/src/test/java/com/tlcsdm/core/powershell/PowerShellTest.java
+++ core/src/test/java/com/tlcsdm/core/powershell/PowerShellTest.java
@@ -31,6 +31,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 import java.io.BufferedReader;
 import java.io.File;
