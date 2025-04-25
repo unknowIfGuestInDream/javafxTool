92ef9ee78f0bcbf235807160bb60370669913430
liang.tang.yk <liang.tang.yk@bp.renesas.com>
2025-04-25 09:58:06 +0800

    ci: Improve CI

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -87,6 +87,7 @@ module com.tlcsdm.core {
     requires org.reflections;
     requires static org.apache.commons.csv;
     requires static org.openjdk.nashorn;
+    requires static jcip.annotations;
 
     opens com.tlcsdm.core.javafx.controller to javafx.fxml;
     opens com.tlcsdm.core.javafx.view to javafx.fxml;
