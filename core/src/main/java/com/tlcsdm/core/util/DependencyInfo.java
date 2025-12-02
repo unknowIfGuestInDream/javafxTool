210b33437b8492b3936ecb60338352eee0701751
梦里不知身是客 <liang.tang.cx@gmail.com>
2025-12-02 09:46:06 +0800

    docs: Add doc

--- core/src/main/java/com/tlcsdm/core/util/DependencyInfo.java
+++ core/src/main/java/com/tlcsdm/core/util/DependencyInfo.java
@@ -105,7 +105,7 @@ public class DependencyInfo {
             new Dependency("com.google.guava", "guava", "33.5.0-jre", false, "https://github.com/google/guava",
                 "Apache License, Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0"),
 
-            new Dependency("com.github.ben-manes.caffeine", "caffeine", "3.2.2", false,
+            new Dependency("com.github.ben-manes.caffeine", "caffeine", "3.2.3", false,
                 "https://github.com/ben-manes/caffeine", "Apache License, Version 2.0",
                 "https://www.apache.org/licenses/LICENSE-2.0"),
 
